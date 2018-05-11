b441ebf016ae3987f521c766af509c098cffaaaa
BIN <shang1219178163@gmail.com>
2018-05-11 17:23:00 +0800

    update

---
+++ SwiftTemplet/SwiftTemplet/BN_TableViewCellZero.swift
@@ -0,0 +1,11 @@
+//
+//  BN_TableViewCellZero.swift
+//  SwiftTemplet
+//
+//  Created by hsf on 2018/5/11.
+//  Copyright © 2018年 BN. All rights reserved.
+//
+
+import Foundation
+
+
--- SwiftTemplet/SwiftTemplet/MainViewController.swift
+++ SwiftTemplet/SwiftTemplet/MainViewController.swift
@@ -9,18 +9,85 @@
 import Foundation
 import UIKit
 
-class MainViewController: UIViewController {
+extension MainViewController{
+    
+    
+    
+}
+
+class MainViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    
+    var dataList : NSMutableArray = [];
+    
     
     override func viewDidLoad() {
         super.viewDidLoad();
         
         title = "Main";
         view.backgroundColor = UIColor.red;
+        
+        
+        self.view.addSubview(self.tableView);
+        
+        
+        for _ in 0...5{
+            let marr : NSMutableArray = [];
+            for j in 0...3{
+                marr.add(j);
+                
+            }
+            self.dataList.add(marr);
+        }
+        print(self.dataList);
+        
+    }
+//    MARK: - tableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.dataList.count;
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let arraySection : NSArray = self.dataList[section] as! NSArray;
+        return arraySection.count;
+    };
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 60;
+    };
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let  identifier : String = "identifier";
+        var  cell = tableView.dequeueReusableCell(withIdentifier: identifier);
+        if cell == nil{
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: identifier);
+        }
+
+        cell?.textLabel?.text = String.init(format: "section_%d,row_%d", indexPath.section,indexPath.row);
+        
+        return cell!;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - layz
+    lazy var tableView: UITableView = {
+        let table = UITableView(frame:self.view.bounds, style:UITableViewStyle.grouped);
+        table.dataSource = self;
+        table.delegate = self;
+        
+        return table;
+    }();
     
 }
