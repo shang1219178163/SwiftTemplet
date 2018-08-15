4139f23ee8d7376798b8ebae3d9e48c30a5d283d
BIN <shang1219178163@gmail.com>
2018-08-15 08:55:02 +0800

    update

---
+++ SwiftTemplet/SwiftTemplet/Expand/Macro_Globle.swift
@@ -0,0 +1,121 @@
+
+//
+//  Macro_Globle.swift
+//  SwiftTemplet
+//
+//  Created by hsf on 2018/8/15.
+//  Copyright © 2018年 BN. All rights reserved.
+//
+
+import UIKit
+
+// MARK: -tag
+
+let kTAG_LABEL = 100;
+let kTAG_BTN = 200;
+let kTAG_BTN_RightItem = 260;
+let kTAG_BTN_BackItem = 261;
+
+let kTAG_IMGVIEW = 300;
+let kTAG_TEXTFIELD = 400;
+let kTAG_TEXTVIEW = 500;
+let kTAG_ALERT_VIEW = 600;
+let kTAG_ACTION_SHEET = 700;
+let kTAG_PICKER_VIEW = 800;
+let kTAG_PICKER_DATE = 900;
+
+let kTAG_VIEW = 1000;
+let kTAG_VIEW_Segment = 1100;
+let kTAG_VIEW_radio = 1200;
+let kTAG_VIEW_Picture = 1300;
+
+
+// MARK: -MacroGeometry与计算有关的尺寸属性
+
+let kH_StatusBar = 20;
+let kH_NaviagtionBar = 44;
+let kH_TabBar = 49;
+
+let kXY_GAP = 15;
+let kX_GAP = 15;
+let kY_GAP = 10;
+let kPadding = 8.0;
+
+let kW_LayerBorderWidth = 0.5;
+let kWH_ArrowRight = 25;
+let kTimerValue = 65;
+let kRatio_IDCard = 1.58;
+let kAnimationDuration_Toast = 1.5;
+let kAnimationDuration_Drop = 0.5;
+
+let kH_CellHeight = 60;
+
+
+let kW_item = 80;
+let kW_progressView = 130;
+
+let kH_LABEL = 25;
+let kH_LABEL_TITLE = 30;
+let kH_LABEL_SMALL = 20;
+
+let kH_TEXTFIELD = 30;
+let kH_LINE_VIEW =  1/3.0;
+let kW_LINE_Vert =  3.0;
+
+
+// MARK: -font
+
+let KFZ_First =  18;
+let KFZ_Second = 16;
+let KFZ_Third =  14;
+let KFZ_Fouth =  12;
+let KFZ_Fifth =  10;
+
+// MARK: -视图
+
+let kIMAGE_arrowRight = "img_arrowRight.png";
+let kIMAGE_arrowDown = "img_arrowDown_black.png";
+let kIMAGE_arrowBack = "img_btnBack.png";
+
+let kIMAGE_default_User = "img_portrait_Default.png";
+let kIMAGE_default_User_N = "img_headPortrait_N.png";
+let kIMAGE_default_User_H = "img_headPortrait_H.png";
+let kIMAGE_defaultAddPhoto = "img_photoAddDefault.png";
+let kIMAGE_photoDelete = "img_Picture_Delete.png";
+
+let kIMAGE_default_failed = "imageFailedDefault.png";
+
+let kIMAGE_SexBoy = "img_sex_boy.png";
+let kIMAGE_SexGril = "img_sex_gril.png";
+
+let kIMAGE_elemetDecrease = "decrease_elemet";
+let kIMAGE_elemetIncrease = "increase_elemet";
+
+let kIMAGE_scan = "img_scan.png";
+let kIMAGE_NFC = "img_NFC.png";
+let kIMAGE_update = "img_dialog_update";
+let kIMAGE_inquiry = "img_dialog_inquiry";
+let kIMAGE_warning = "img_dialog_warning";
+
+
+let kMsg_NetWorkRequesting = "网络请求中...";
+let kMsg_NetWorkFailed = "网络请求失败,请稍后再试";
+let kMsg_NetWorkNodata = "暂无数据";
+let kMsg_NetWorkNoMoredata = "没有更多数据了";
+let kMsg_NetWorkFailed_Params = "参数错误,请稍后再试";
+
+let kMsg_Locationing = "定位中...";
+let kMsg_LocationSuccess = "位置信息更新成功!";
+let kMsg_LocationFailed = "定位失败,请稍后再试";
+let kMsg_IDCardFailed = "身份证识别失败,请稍后再试";
+let kMsg_IDCardSuccess = "身份证识别成功";
+
+
+let kActionTitle_Know = "知道了";
+let kActionTitle_Sure = "确定";
+let kActionTitle_Cancell = "取消";
+let kActionTitle_Delete = "删除";
+let kActionTitle_Drop = "彻底删除";
+
+let kActionTitle_Collect = "收藏";
+let kActionTitle_Recover = "恢复";
