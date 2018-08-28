3f5b676015da8463d3870fdbc9d544c0c24746d0
BIN <shang1219178163@gmail.com>
2018-08-28 15:25:22 +0800

    update

--- SwiftTemplet/SwiftTemplet/Expand/NSString+Helper.swift
+++ SwiftTemplet/SwiftTemplet/Expand/NSString+Helper.swift
@@ -10,10 +10,42 @@ import UIKit
 
 extension String{
     
+    
     static func timeNow() -> String {
         let fmt = DateFormatter.dateFormat(formatStr:kFormat_date);
         let dateStr = fmt.string(from: Date());
         return dateStr;
+        
+    }
+    
+    func toTimeStamp() -> String {
+        let dateStr = self;
+        
+        var fmtStr = kFormat_date;
+        if dateStr.contains("-") && dateStr.contains(":") {
+            fmtStr = kFormat_date;
+            
+        }
+        else if dateStr.contains("-") && !dateStr.contains(":") {
+            fmtStr = kFormat_date_one;
+            
+        }
+        else if !dateStr.contains("-") && !dateStr.contains(":") {
+            fmtStr = kFormat_date_two;
+            
+        }
+        else{
+            DDLog("<\(dateStr)>时间格式不对");
+            
+        }
+        
+        let fmt = DateFormatter.dateFormat(formatStr: fmtStr);
+        let date = fmt.date(from: dateStr);
+        
+        let intervl:Double = (date?.timeIntervalSince1970)!;
+        let doubleInt = Int(intervl);
+        let timeStamp = String(doubleInt);
+        return timeStamp;
         
     }
     
