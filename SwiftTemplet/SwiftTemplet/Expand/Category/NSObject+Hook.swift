c52558deff4df60a7ba7f7caae57b524dc932c07
BIN Shang <shang1219178163@gmail.com>
2018-12-12 13:20:39 +0800

    update

---
+++ SwiftTemplet/SwiftTemplet/Expand/Category/NSObject+Hook.swift
@@ -0,0 +1,29 @@
+//
+//  Hook.swift
+//  SwiftTemplet
+//
+//  Created by dev on 2018/12/11.
+//  Copyright © 2018年 BN. All rights reserved.
+//
+
+import UIKit
+
+extension NSObject{
+    
+    
+    func MethodSwizzle(origSel:Selector, newSel:Selector) -> Bool {
+        
+        let originalMethod = class_getInstanceMethod(self.classForCoder, origSel);
+        let swizzledMethod = class_getInstanceMethod(self.classForCoder, newSel);
+        //在进行 Swizzling 的时候,需要用 class_addMethod 先进行判断一下原有类中是否有要替换方法的实现
+        let didAddMethod: Bool = class_addMethod(self.classForCoder, originalSelector, method_getImplementation(swizzledMethod!), method_getTypeEncoding(swizzledMethod!))
+        //如果 class_addMethod 返回 yes,说明当前类中没有要替换方法的实现,所以需要在父类中查找,这时候就用到 method_getImplemetation 去获取 class_getInstanceMethod 里面的方法实现,然后再进行 class_replaceMethod 来实现 Swizzing
+        
+        if didAddMethod {
+            class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
+        } else {
+            method_exchangeImplementations(originalMethod!, swizzledMethod!)
+        }
+    }
+    
+}
