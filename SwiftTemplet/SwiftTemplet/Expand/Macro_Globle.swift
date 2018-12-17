ad0188c6caafa645e6622e01384b60145b7b17a6
Shang <shang1219178163@gmail.com>
2018-12-17 10:42:57 +0800

    update

--- SwiftTemplet/SwiftTemplet/Expand/Macro_Globle.swift
+++ SwiftTemplet/SwiftTemplet/Expand/Macro_Globle.swift
@@ -78,34 +78,44 @@ let kFZ_Fifth =  10;
 
 // MARK: -视图
 
+let kIMG_arrowRight = "img_arrowRight_gray";
+let kIMG_arrowDown = "img_arrowDown_black";
+let kIMG_arrowBack = "img_arrowLeft_white";
+let kIMG_arrowUp = "img_arrowUp_blue";
 
-let kIMG_arrowUp = "img_arrowUp";
-let kIMG_arrowLeft = "img_arrowLeft";
-let kIMG_arrowDown = "img_arrowDown";
-let kIMG_arrowRight = "img_arrowRight";
-let kIMG_arrowBack = "img_arrowLeft_88_w";
+let kIMG_portrait = "img_portrait_N";
+let kIMG_portrait_N = "img_portrait_N";
+let kIMG_portrait_H = "img_portrait_H";
+let kIMG_pictureAdd = "img_pictureAdd";
+let kIMG_pictureDelete = "img_pictureDelete";
 
+let kIMG_defaultFailed = "img_failedDefault";
+let kIMG_defaultFailed_S = "img_failedDefault_S";
+let kIMG_defaultPortrait = "img_portrait_N";
 
-let kIMG_default_User = "img_portrait_Default";
-let kIMG_default_User_N = "img_headPortrait_N";
-let kIMG_default_User_H = "img_headPortrait_H";
-let kIMG_defaultAddPhoto = "img_photoAddDefault";
-let kIMG_photoDelete = "img_Picture_Delete";
+let kIMG_sexBoy = "img_sex_boy";
+let kIMG_sexGril = "img_sex_gril";
 
-let kIMG_default_failed = "imageFailedDefault";
-
-let kIMG_SexBoy = "img_sex_boy";
-let kIMG_SexGril = "img_sex_gril";
-
-let kIMG_elemetDecrease = "decrease_elemet";
-let kIMG_elemetIncrease = "increase_elemet";
+let kIMG_elemetDec = "img_elemet_decrease";
+let kIMG_elemetInc = "img_elemet_increase";
 
 let kIMG_scan = "img_scan";
 let kIMG_NFC = "img_NFC";
-let kIMG_update = "img_dialog_update";
+
 let kIMG_inquiry = "img_dialog_inquiry";
+let kIMG_update = "img_dialog_update";
 let kIMG_warning = "img_dialog_warning";
 
+let kIMG_notice = "img_notice";
+let kIMG_location_H = "img_location_H";
+let kIMG_more = "img_more";
+
+let kIMG_selected_NO = "btn_selected_NO";
+let kIMG_selected_YES = "btn_selected_YES";
+let kIMG_Add = "btn_add";
+
+let kIMG_like_H = "img_like_H";
+let kIMG_like_W = "img_like_W";
 
 let kMsg_NetWorkRequesting = "网络请求中...";
 let kMsg_NetWorkFailed = "网络请求失败,请稍后再试";
@@ -125,6 +135,15 @@ let kActionTitle_Sure = "确定";
 let kActionTitle_Cancell = "取消";
 let kActionTitle_Delete = "删除";
 let kActionTitle_Drop = "彻底删除";
+let kActionTitle_Call = "呼叫";
+let kActionTitle_Update = "立即升级";
 
 let kActionTitle_Collect = "收藏";
 let kActionTitle_Recover = "恢复";
+
+
+//MARK: -通用
+
+let kDes_week              =   "星期一,星期二,星期三,星期四,星期五,星期六,星期天";
+let kDes_month             =   "正月, 二月, 三月, 四月, 五月, 六月, 七月, 八月,九月, 十月, 冬月, 腊月";
+let kDes_day              =   "初一, 初二, 初三, 初四, 初五, 初六, 初七, 初八, 初九, 初十,十一, 十二, 十三, 十四, 十五, 十六, 十七, 十八, 十九, 二十, 廿一, 廿二, 廿三, 廿四, 廿五, 廿六, 廿七, 廿八, 廿九, 三十, 三十一";
