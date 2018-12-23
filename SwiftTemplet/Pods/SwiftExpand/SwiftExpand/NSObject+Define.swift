755f81c57c65db01192059b2d8fed2bec69f593a
Shang <shang1219178163@gmail.com>
2018-12-23 18:41:55 +0800

    update

--- SwiftTemplet/Pods/SwiftExpand/SwiftExpand/NSObject+Define.swift
+++ SwiftTemplet/Pods/SwiftExpand/SwiftExpand/NSObject+Define.swift
@@ -13,6 +13,7 @@ public typealias SwiftBlock = (AnyObject,AnyObject,Int);
 public typealias ObjClick = ((AnyObject?) ->()) // 定义数据类型(其实就是设置别名)
 public typealias ViewClick = ((UITapGestureRecognizer?,UIView,NSInteger)->()) // 定义数据类型(其实就是设置别名)
 public typealias ControlClick = (UIControl) ->() // 定义数据类型(其实就是设置别名)
+public typealias RecognizerClick = (UIGestureRecognizer) ->()
 
 // MARK: - 关联属性的key
 public struct RuntimeKey {
@@ -21,6 +22,14 @@ public struct RuntimeKey {
     public static let tap = UnsafeRawPointer.init(bitPattern: "tap".hashValue)!;
     public static let item = UnsafeRawPointer.init(bitPattern: "item".hashValue)!;
     public static let control = UnsafeRawPointer.init(bitPattern: "control".hashValue)!;
+    public static let recognizerTap = UnsafeRawPointer.init(bitPattern: "recognizerTap".hashValue)!;
+    public static let recognizerSwip = UnsafeRawPointer.init(bitPattern: "recognizerSwip".hashValue)!;
+    public static let recognizerLongPress = UnsafeRawPointer.init(bitPattern: "recognizerLongPress".hashValue)!;
+    public static let recognizerPan = UnsafeRawPointer.init(bitPattern: "recognizerPan".hashValue)!;
+    public static let recognizerPinch = UnsafeRawPointer.init(bitPattern: "recognizerPinch".hashValue)!;
+    public static let recognizerRotation = UnsafeRawPointer.init(bitPattern: "recognizerRotation".hashValue)!;
+    public static let recognizerEdgPan = UnsafeRawPointer.init(bitPattern: "recognizerEdgPan".hashValue)!;
+   
 
 }
 
