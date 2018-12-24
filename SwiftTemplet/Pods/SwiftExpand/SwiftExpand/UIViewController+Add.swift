a4364d7797711c0cc420759aad332e443d0a59b3
Shang <shang1219178163@gmail.com>
2018-12-24 09:51:44 +0800

    update

--- SwiftTemplet/Pods/SwiftExpand/SwiftExpand/UIViewController+Add.swift
+++ SwiftTemplet/Pods/SwiftExpand/SwiftExpand/UIViewController+Add.swift
@@ -57,6 +57,43 @@ public extension UIViewController{
         }
     }
     
- 
+    public var collectionView : UICollectionView {
+        get {
+            var view = objc_getAssociatedObject(self, AssociationKeyFromSelector(#function)) as? UICollectionView;
+            if view == nil {
+                // 初始化
+                let layout = UICollectionViewFlowLayout()
+                let itemW = (kScreenWidth - 5*5.0)/4.0
+                layout.itemSize = CGSize(width: itemW, height: itemW)
+                layout.minimumLineSpacing = 5
+                layout.minimumInteritemSpacing = 5
+                //        layout.scrollDirection = .vertical
+                layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+                // 设置分区头视图和尾视图宽高
+                layout.headerReferenceSize = CGSize(width: kScreenWidth, height: 60)
+                layout.footerReferenceSize = CGSize(width: kScreenWidth, height: 60)
+                
+                view = UICollectionView(frame: CGRect(x:0, y:64, width:kScreenWidth, height:400), collectionViewLayout: layout)
+                view!.backgroundColor = UIColor.background
+                
+                if self.conforms(to: UICollectionViewDelegate.self) {
+                    view!.delegate = (self as! UICollectionViewDelegate)
+                }
+                if self.conforms(to: UICollectionViewDataSource.self) {
+                    view!.dataSource = (self as! UICollectionViewDataSource)
+                }
+             
+                view!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: UICollectionViewCell.identifier)
+              
+                view!.backgroundColor = UIColor.background;
+           
+                objc_setAssociatedObject(self, AssociationKeyFromSelector(#function), view, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+            }
+            return view!;
+        }
+        set {
+            objc_setAssociatedObject(self, AssociationKeyFromSelector(#function), newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
+    }
 
 }
