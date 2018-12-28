7048a70d3ebb341d2e966bf8d081907576574605
Shang <shang1219178163@gmail.com>
2018-12-28 19:58:42 +0800

    update

--- SwiftTemplet/SwiftTemplet/AppDelegate.swift
+++ SwiftTemplet/SwiftTemplet/AppDelegate.swift
@@ -23,8 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UIViewController.initializeMethod();
         UINavigationController.initializeMethod();
-        
-        
+        UIImageView.initializeMethod()
+
         // Override point for customization after application launch.
         
         //方式一
