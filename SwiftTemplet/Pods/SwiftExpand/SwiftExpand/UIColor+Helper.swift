54effb5d7dce940adbb8d09609de74068eec3d0f
Shang <shang1219178163@gmail.com>
2019-01-02 18:37:18 +0800

    update

--- SwiftTemplet/Pods/SwiftExpand/SwiftExpand/UIColor+Helper.swift
+++ SwiftTemplet/Pods/SwiftExpand/SwiftExpand/UIColor+Helper.swift
@@ -11,64 +11,17 @@ import UIKit
 
 public extension UIColor{
     
-    public static func hex(_ hex:String) -> UIColor {
-        var cString = hex.trimmingCharacters(in: CharacterSet.whitespaces).uppercased();
-        if cString.hasPrefix("#") {
-            let index = cString.index(cString.startIndex, offsetBy:1);
-            cString = String(cString[index...]);
-        }
-        
-        if cString.count != 6 {
-            return .red;
-        }
-        
-        let rIndex = cString.index(cString.startIndex, offsetBy: 2);
-        let rString = String(cString[..<rIndex]);
-        
-        let otherString = String(cString[rIndex...]);
-        let gIndex = otherString.index(otherString.startIndex, offsetBy: 2);
-        let gString = String(otherString[..<gIndex]);
-        
-        let bIndex = cString.index(cString.endIndex, offsetBy: -2);
-        let bString = String(cString[bIndex...]);
-        
-        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0
-        Scanner(string: rString).scanHexInt32(&r);
-        Scanner(string: gString).scanHexInt32(&g);
-        Scanner(string: bString).scanHexInt32(&b);
-        
-//        print(hex,rString,gString,bString,otherString)
-        return UIColor(red:CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: CGFloat(1.0));
-    }
-    
-    public static func UIColorFromHex(_ hex:String) -> UIColor {
-        return UIColor.hex(hex);
-    }
-    
-    public static func UIColorFromrRandom() -> UIColor {
-        return UIColor.randomColor();
-    }
-    
-    public static func randomColor() -> UIColor {
-    
-        let r = arc4random_uniform(256);
-        let g = arc4random_uniform(256);
-        let b = arc4random_uniform(256);
-        return UIColor(red:CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: CGFloat(1.0));
-
-    }
-        
     //MARK: - -属性
     public static var random : UIColor {
         get{
             return UIColor.randomColor();
-            
         }
     }
     
-   public static var theme : UIColor {
+    public static var theme : UIColor {
         get{
-            return UIColor.hex("#0082e0");
+//            return UIColor.hex("#0082e0");
+            return UIColor.orange;
         }
     }
         
@@ -108,8 +61,42 @@ public extension UIColor{
         }
     }
     
-    public static func RGBA (_ r:CGFloat,_ g:CGFloat,_ b:CGFloat,_ a:CGFloat) -> UIColor{
-        return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
+    //MARK: - -方法
+    public static func hex(_ hex:String) -> UIColor {
+        var cString = hex.trimmingCharacters(in: CharacterSet.whitespaces).uppercased();
+        if cString.hasPrefix("#") {
+            let index = cString.index(cString.startIndex, offsetBy:1);
+            cString = String(cString[index...]);
+        }
+        
+        if cString.count != 6 {
+            return .red;
+        }
+        
+        let rIndex = cString.index(cString.startIndex, offsetBy: 2);
+        let rString = String(cString[..<rIndex]);
+        
+        let otherString = String(cString[rIndex...]);
+        let gIndex = otherString.index(otherString.startIndex, offsetBy: 2);
+        let gString = String(otherString[..<gIndex]);
+        
+        let bIndex = cString.index(cString.endIndex, offsetBy: -2);
+        let bString = String(cString[bIndex...]);
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0
+        Scanner(string: rString).scanHexInt32(&r);
+        Scanner(string: gString).scanHexInt32(&g);
+        Scanner(string: bString).scanHexInt32(&b);
+        
+        //        print(hex,rString,gString,bString,otherString)
+        return UIColor(red:CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: CGFloat(1.0));
+    }
+    
+    public static func randomColor() -> UIColor {
+        let r = arc4random_uniform(256);
+        let g = arc4random_uniform(256);
+        let b = arc4random_uniform(256);
+        return UIColor(red:CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: CGFloat(1.0));
     }
     
 }
