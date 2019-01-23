c42412659d8fc8aaf810f15da23327f9f5760a17
Shang <shang1219178163@gmail.com>
2019-01-23 11:51:17 +0800

    update

--- SwiftTemplet/SwiftTemplet/View/UITableViewCell/UITableViewCellTextField.swift
+++ SwiftTemplet/SwiftTemplet/View/UITableViewCell/UITableViewCellTextField.swift
@@ -57,7 +57,7 @@ class UITableViewCellTextField: UITableViewCell,UITextFieldDelegate {
         labelLeft.sizeToFit()
         labelLeft.frame.size = CGSize(width: labelLeft.frame.width, height: 35)
         labelLeft.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(contentView.frame.midY - labelLeft.frame.height/2.0)
+            make.centerY.equalToSuperview()
             make.left.equalToSuperview().offset(kX_GAP)
             make.size.equalTo(labelLeft.size);
         }
