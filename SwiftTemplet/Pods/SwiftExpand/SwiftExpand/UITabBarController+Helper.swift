4313198e153eabf4c0763d94356d1f89c86bcf43
Shang <shang1219178163@gmail.com>
2019-02-13 18:02:49 +0800

    update

---
+++ SwiftTemplet/Pods/SwiftExpand/SwiftExpand/UITabBarController+Helper.swift
@@ -0,0 +1,35 @@
+//
+//  UITabBarController+Helper.swift
+//  SwiftExpand
+//
+//  Created by Bin Shang on 2019/2/13.
+//
+
+import UIKit
+
+public extension UITabBarController{
+    
+    func getSubviewsForName(_ name: String) -> [UIView] {
+        var marr: [UIView] = []
+        tabBar.subviews.forEach { (view) in
+            if view.isKind(of: SwiftClassFromString(name)) {
+                marr.append(view)
+            }
+        }
+        return marr;
+    }
+    
+    /// 用特定数据源刷新tabBar
+    /// - Parameter list: 参照HomeViewController数据源
+    func reloadTabarItems(_ list: [[Any]]) -> Void {
+        
+        for e in viewControllers!.enumerated(){
+            let itemList = list[e.offset]
+            let title = itemList[itemList.count - 4] as! String
+            let img = UIImage(named: itemList[itemList.count - 3] as! String)
+            let imgH = UIImage(named: itemList[itemList.count - 2] as! String)
+            e.element.tabBarItem = UITabBarItem(title: title, image: img, selectedImage: imgH)
+        }
+    }
+    
+}
