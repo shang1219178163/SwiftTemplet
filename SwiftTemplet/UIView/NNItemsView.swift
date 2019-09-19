6650ea390df0ee478889ed6e910a92341eadc7a2
Shang <shang1219178163@gmail.com>
2019-09-19 19:13:11 +0800

    优化升级

--- SwiftTemplet/UIView/NNItemsView.swift
+++ SwiftTemplet/UIView/NNItemsView.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class NNItemsView: UIView {
 
+    var cornerRadius: CGFloat = 5.0
     var numberOfRow: Int = 4
     var padding: CGFloat = 5.0
+    var lineColor: UIColor = UIColor.line
+
+    var isMutiChoose: Bool = false;
+    var selectedList: [UIButton] = []
+    var selectedIdxList: [Int] = []{
+        willSet{
+            if newValue.count <= 0 {
+                return;
+            }
+            if isMutiChoose == false && newValue.count > 1 {
+                fatalError("单选只能默认选择索引应该为单选");
+            }
+        }
+    }
+
     var items:[String]?
     {
         willSet{
@@ -22,21 +38,64 @@ class NNItemsView: UIView {
             
             for e in newValue!.enumerated() {
                 let view = createBtn(rect: .zero, title: e.element, tag: e.offset);
-                view.addActionHandler({ (sender) in
-                    if self.viewBlock != nil {
-                        self.viewBlock!(self, sender)
-                    }
-                }, for: .touchUpInside)
+                view.addTarget(self, action: #selector(handleAction(_:)), for: .touchUpInside)
                 addSubview(view)
                 
                 itemList.append(view)
             }
         }
     }
-//    var type :Int = 0
+    
+    @objc func handleAction(_ sender: UIButton) -> Void {
+        sender.isSelected = !sender.isSelected;
+        
+        if sender.isSelected == true {
+            sender.setTitleColor(UIColor.theme, for: .normal)
+            sender.layer.borderColor = UIColor.theme.cgColor;
+            if self.isMutiChoose == false {
+                for e in self.selectedList.enumerated() {
+                    e.element.isSelected = false;
+                    e.element.setTitleColor(UIColor.gray, for: .normal)
+                    e.element.layer.borderColor = lineColor.cgColor;
+                }
+                self.selectedList.removeAll()
+                self.selectedList.append(sender)
+            } else {
+                self.selectedList.append(sender)
+            }
+        } else {
+            sender.setTitleColor(UIColor.gray, for: .normal)
+            sender.layer.borderColor = lineColor.cgColor;
+            if self.selectedList.contains(sender) == true {
+                self.selectedList.remove(at: self.selectedList.firstIndex(of: sender)!)
+            }
+        }
+        print(sender.isSelected, self.selectedList)
+        
+        if self.viewBlock != nil {
+            self.viewBlock!(self, sender)
+        }
+    }
     
     var viewBlock: ((NNItemsView, UIControl) -> Void)?
-    var itemList: [UIButton] = []
+    var itemList: [UIButton] = []{
+        willSet{
+            if newValue.count <= 0 {
+                return;
+            }
+            subviews.forEach { (subview) in
+                subview.removeFromSuperview()
+            }
+            
+            for e in newValue.enumerated() {
+                addSubview(e.element)
+                if selectedIdxList.contains(e.offset) == true {
+                    selectedList.append(e.element);
+                }
+            }
+            setupConstraint()
+        }
+    }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -50,47 +109,47 @@ class NNItemsView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        if items != nil {
-            setupConstraint()
-        }
+        setupConstraint()
     }
     
     func setupConstraint() -> Void {
-        let rowCount = items!.count % numberOfRow == 0 ? items!.count/numberOfRow : items!.count/numberOfRow + 1;
+        if itemList.count == 0 || bounds.width <= 0 {
+            return;
+        }
+        
+        let rowCount = itemList.count % numberOfRow == 0 ? itemList.count/numberOfRow : itemList.count/numberOfRow + 1;
         let itemWidth = (bounds.width - CGFloat(numberOfRow - 1)*padding)/CGFloat(numberOfRow)
         let itemHeight = (bounds.height - CGFloat(rowCount - 1)*padding)/CGFloat(rowCount)
         
-        for e in items!.enumerated() {
+        for e in itemList.enumerated() {
             let x = CGFloat(e.offset % numberOfRow) * (itemWidth + padding)
             let y = CGFloat(e.offset / numberOfRow) * (itemHeight + padding)
             let rect = CGRect(x: x, y: y, width: itemWidth, height: itemHeight)
             
-            let view = self.itemList[e.offset];
+            let view = e.element;
             view.frame = rect;
+            if self.cornerRadius > 0 {
+                view.layer.cornerRadius = self.cornerRadius;
+                view.layer.masksToBounds = true;
+            }
+            
+            view.layer.borderWidth = 1;
+            if selectedList.contains(view) == true {
+                view.setTitleColor(UIColor.theme, for: .normal)
+                view.layer.borderColor = UIColor.theme.cgColor;
+            } else {
+                view.setTitleColor(UIColor.gray, for: .normal)
+                view.layer.borderColor = lineColor.cgColor;
+            }
+         
         }
     }
     
-//    func setupConstraint() -> Void {
-//
-//        let rowCount = items!.count % numberOfRow == 0 ? items!.count/numberOfRow : items!.count/numberOfRow + 1;
-//        let itemWidth = (bounds.width - CGFloat(numberOfRow - 1)*padding)/CGFloat(numberOfRow)
-//        let itemHeight = (bounds.height - CGFloat(rowCount - 1)*padding)/CGFloat(rowCount)
-//
-//        for e in items!.enumerated() {
-//            let x = CGFloat(e.offset % numberOfRow) * (itemWidth + padding)
-//            let y = CGFloat(e.offset / numberOfRow) * (itemHeight + padding)
-//            let rect = CGRect(x: x, y: y, width: itemWidth, height: itemHeight)
-//
-//            let view = createBtn(rect: rect, title: e.element, tag: e.offset);
-//            view.backgroundColor = UIColor.theme;
-//            view.addActionHandler({ (sender) in
-//                if self.viewBlock != nil {
-//                    self.viewBlock!(self, sender)
-//                }
-//            }, for: .touchUpInside)
-//            addSubview(view)
-//        }
-//    }
+    // MARK: -funtions
+    func itemViewHeight(_ itemHeight: CGFloat = 40) -> CGFloat {
+        let rowCount = itemList.count % numberOfRow == 0 ? itemList.count/numberOfRow : itemList.count/numberOfRow + 1;
+        return rowCount.toCGFloat * itemHeight + (rowCount - 1).toCGFloat * padding;
+    }
     
     func block(_ action: @escaping ((NNItemsView, UIControl) -> Void)) -> Void {
         viewBlock = action;
