89107128e2b9c698cbfd4d214550912e7b83ae72
Shang <shang1219178163@gmail.com>
2019-09-21 16:05:03 +0800

    update

--- SwiftTemplet/Controllers/TestViewController.swift
+++ SwiftTemplet/Controllers/TestViewController.swift
@@ -20,17 +20,34 @@ class TestViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        DDLog(NSArray.range(0, 10, 1));
-        DDLog(NSArray.range(1, 11, 1));
-        DDLog(NSArray.range(0, 30, 5));
-        DDLog(NSArray.range(-10, 0, 1));
+//        DDLog(NSArray.range(0, 10, 1));
+//        DDLog(NSArray.range(1, 11, 1));
+//        DDLog(NSArray.range(0, 30, 5));
+//        DDLog(NSArray.range(-10, 0, 1));
 
+        DDLog("\("小刚".transformToPinyin())")
+        DDLog("\("韩梅梅".transformToPinyin())")
+
+        let array = [1,2,3,4,5,6,7]
+        let list = array.shuffled()
+        DDLog(list)
+        array.randomElement()
     }
-
+    
+    
 }
 
-public extension NSArray{
- 
+public extension String{
+    func transformToPinyin() -> String {
+        let chinese: String = self;
+        let mutableStr = NSMutableString(string: chinese) as CFMutableString
+        let canTransform: Bool = CFStringTransform(mutableStr, nil, kCFStringTransformToLatin, false) &&
+            CFStringTransform(mutableStr, nil, kCFStringTransformStripCombiningMarks, false);
+        if canTransform == true {
+            return mutableStr as String
+        }
+        return ""
+    }
     
     
 }
