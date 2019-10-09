7775736039d5ee5f338a1d12371930b860b0bfa8
Shang <shang1219178163@gmail.com>
2019-10-09 14:35:50 +0800

    新增

---
+++ SwiftTemplet/Controllers/UITableViewCellPark.swift
@@ -0,0 +1,24 @@
+//
+//  UITableViewCellPark.swift
+//  CloudCustomerService
+//
+//  Created by Bin Shang on 2019/10/8.
+//  Copyright © 2019 Xi'an iRain IoT. Technology Service CO., Ltd. . All rights reserved.
+//
+
+import UIKit
+
+class UITableViewCellPark: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
