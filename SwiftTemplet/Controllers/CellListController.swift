46325ec6289f694c088fd5c9c84d10bad5a0492d
Shang <shang1219178163@gmail.com>
2019-10-09 14:35:50 +0800

    新增

---
+++ SwiftTemplet/Controllers/CellListController.swift
@@ -0,0 +1,30 @@
+//
+//  CellListController.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2019/10/8.
+//  Copyright © 2019 BN. All rights reserved.
+//
+
+import UIKit
+
+class CellListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
