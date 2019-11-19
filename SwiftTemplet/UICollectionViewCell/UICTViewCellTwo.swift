10a7ea4b49e6c8866e7d0b6b1d304481bbe5d9f9
Shang <shang1219178163@gmail.com>
2019-11-19 19:41:53 +0800

    新视图

---
+++ SwiftTemplet/UICollectionViewCell/UICTViewCellTwo.swift
@@ -0,0 +1,13 @@
+//
+//  UICTViewCellTwo.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2019/11/19.
+//  Copyright © 2019 BN. All rights reserved.
+//
+
+import UIKit
+
+class UICTViewCellTwo: UICollectionViewCell {
+    
+}
