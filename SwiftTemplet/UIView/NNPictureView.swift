374b7fb923d6c1ec557e68d2c5376552bceccdf3
Shang <shang1219178163@gmail.com>
2019-11-19 19:41:53 +0800

    新视图

---
+++ SwiftTemplet/UIView/NNPictureView.swift
@@ -0,0 +1,21 @@
+//
+//  NNPictureView.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2019/11/19.
+//  Copyright © 2019 BN. All rights reserved.
+//
+
+import UIKit
+
+class NNPictureView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
