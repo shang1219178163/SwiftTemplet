b5b21671d70955d5836f85b20bea2a327535b06d
Shang <shang1219178163@gmail.com>
2019-12-06 13:21:03 +0800

    改为pod导入

---
+++ SwiftTemplet/Controllers/PlateKeybordController.swift
@@ -0,0 +1,30 @@
+//
+//  PlateKeybordController.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2019/12/6.
+//  Copyright © 2019 BN. All rights reserved.
+//
+
+import UIKit
+
+class PlateKeybordController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
