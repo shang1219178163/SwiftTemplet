e473d8f0ed7752ff8329344622d61af1b5ccc81c
Shang <shang1219178163@gmail.com>
2020-02-07 17:01:54 +0800

    代码分离

---
+++ SwiftTemplet/Controllers/UITableViewCellWebView.swift
@@ -0,0 +1,24 @@
+//
+//  UITableViewCellWebView.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/2/6.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewCellWebView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
