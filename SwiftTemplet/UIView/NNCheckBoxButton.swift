0d8c9123337ecf305a70a0cae2c63eeb5367d05b
Shang <shang1219178163@gmail.com>
2020-03-04 16:25:11 +0800

    单选框按钮重构

--- SwiftTemplet/UIView/NNCheckBoxButton.swift
+++ SwiftTemplet/UIView/NNCheckBoxButton.swift
@@ -8,17 +8,16 @@
 
 import UIKit
 
+/// 单选框
 class NNCheckBoxButton: UIButton {
 
     var normlImage: UIImage = UIImage(named: "photo_cancell")!
     var seletedImage: UIImage = UIImage(named: "photo_select")!
 
-    var normlTextColor: UIColor = UIColor.black.withAlphaComponent(0.5)
+    var normlTextColor: UIColor = UIColor.black.withAlphaComponent(0.3)
     var seletedTextColor: UIColor = UIColor.theme
     
-    deinit {
-        removeObserver(self, forKeyPath: "selected")
-    }
+    var isImageRight: Bool = false
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -28,42 +27,23 @@ class NNCheckBoxButton: UIButton {
         super.init(frame: frame)
         
         setImage(normlImage, for: .normal)
+        setImage(seletedImage, for: .selected)
+        
         setTitleColor(normlTextColor, for: .normal)
+        setTitleColor(seletedTextColor, for: .selected)
 
         adjustsImageWhenHighlighted = false
-        addObserver(self, forKeyPath: "selected", options: .new, context: nil)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        layoutButton(style: 1, imageTitleSpace: 2)
+        sizeToFit()
         
+        let type = isImageRight ? 3 : 1
+        layoutButton(style: type, imageTitleSpace: 2)
     }
     
     //MARK: -funtions
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        //        DDLog("change_\(change)");
-        if keyPath == "selected" {
-            if let value = change![NSKeyValueChangeKey.newKey] as? NSNumber {
-                let image: UIImage = value.boolValue == true ? seletedImage : normlImage;
-                self.setImage(image, for: .normal)
-                
-                let textColor = value.boolValue == true ? seletedTextColor : normlTextColor;
-                self.setTitleColor(textColor, for: .normal)
-            }
-        }
-    }
-    
-//    override func setTitle(_ title: String?, for state: UIControl.State) {
-//        super.setTitle(title, for: state)
-//        setTitleColor(normlTextColor, for: .normal)
-//
-//    }
-    
-    //    func checkbox(_ title: String?) -> UIButton {
-    //
-    //
-    //    }
     
 }
