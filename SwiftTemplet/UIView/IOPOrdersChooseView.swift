bdc98591c43ee9a76c5af36f26ea0b9117cf3239
Shang <shang1219178163@gmail.com>
2020-03-04 16:25:11 +0800

    单选框按钮重构

---
+++ SwiftTemplet/UIView/IOPOrdersChooseView.swift
@@ -0,0 +1,21 @@
+//
+//  IOPOrdersChooseView.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/3/4.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+
+class IOPOrdersChooseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
