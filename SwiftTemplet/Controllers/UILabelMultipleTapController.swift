cfa7e8570463c4c1168a4c8381254f6ee379227e
Shang <shang1219178163@gmail.com>
2020-04-17 18:09:52 +0800

    标签高亮点击动作

--- SwiftTemplet/Controllers/UILabelMultipleTapController.swift
+++ SwiftTemplet/Controllers/UILabelMultipleTapController.swift
@@ -17,11 +17,8 @@ class UILabelMultipleTapController: UIViewController {
         label.numberOfLines = 1
         label.adjustsFontSizeToFitWidth = true
 //        label.lineBreakMode = .byCharWrapping
-        label.backgroundColor = UIColor.red
         return label
     }()
-
-    let textTaps = ["Terms & Conditions", "Privacy Policy"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,23 +28,14 @@ class UILabelMultipleTapController: UIViewController {
         
         
         label.frame = CGRect(x: 20, y: 20, width: view.frame.width - 40, height: 50)
-        label.text = "By signing up you agree to our \(textTaps[0]) and \(textTaps[1])"
-        let text = (label.text)!
-        let mAttString = NSMutableAttributedString(string: text)
         
-        
-        
-        let range = (text as NSString).range(of: textTaps[0])
-        mAttString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range)
-        
-        let range1 = (text as NSString).range(of: textTaps[1])
-        mAttString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range1)
-        label.attributedText = mAttString
-        
-        
+        let tapTexts = ["智慧停车场平台协议", "用户隐私协议"]
+        label.text = "已阅读并接受《\(tapTexts[0])》和《\(tapTexts[1])》"
+        label.attributedText = NSAttributedString.attString(label.text, textTaps: tapTexts)
+                
         _ = label.addGestureTap { (reco) in
-            guard let sender = reco as? UITapGestureRecognizer else { return }
-            sender.didTapAttributedTextIn(label: self.label, tapTexts: self.textTaps) { (text, idx) in
+            guard let sender = reco as? UITapGestureRecognizer, let label = reco.view as? UILabel else { return }
+            sender.didTapAttributedTextIn(label: label, tapTexts: tapTexts) { (text, idx) in
                 DDLog(text, idx)
             }
         }
@@ -56,26 +44,6 @@ class UILabelMultipleTapController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-    }
-    
-    @objc func handleTap(_ sender: UITapGestureRecognizer) {
-        
-        sender.didTapAttributedTextIn(label: label, tapTexts: textTaps) { (text, idx) in
-            DDLog(text, idx)
-        }
-
-        return
-        let result = sender.didTapAttributedTextIn(label: label, tapText: textTaps[0])
-        let result1 = sender.didTapAttributedTextIn(label: label, tapText: textTaps[1])
-        
-        if result == true {
-            print(textTaps[0])
-
-        } else if result1 == true {
-            print(textTaps[1])
-        } else {
-            print("Tapped none")
-        }
     }
 
 }
