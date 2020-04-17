dbbd6c6cf48420c0659a840af84e3fee6f332a8e
Shang <shang1219178163@gmail.com>
2020-04-17 14:36:09 +0800

    代码解耦

--- SwiftTemplet/UITapGestureRecognizer+Helper.swift
+++ SwiftTemplet/UITapGestureRecognizer+Helper.swift
@@ -51,5 +51,41 @@ import UIKit
             }
         }
     }
+    
+    func didTapAttributedTextIn(label: UILabel, tapText: String) -> Bool {
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: label.attributedText!)
 
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x:(labelSize.width - textBoundingBox.size.width)*0.5 - textBoundingBox.origin.x,
+                                          y:(labelSize.height - textBoundingBox.size.height)*0.5 - textBoundingBox.origin.y);
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x,
+                                                     y: locationOfTouchInLabel.y - textContainerOffset.y);
+
+        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer,
+                                                            in: textContainer,
+                                                            fractionOfDistanceBetweenInsertionPoints: nil)
+
+  
+        guard let text = label.text else { return false }
+        let targetRange: NSRange = (text as NSString).range(of: tapText)
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+    
 }
