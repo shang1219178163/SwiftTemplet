096867091e75250058e62ca5d19a8ec4f7a17a3d
Shang <shang1219178163@gmail.com>
2020-04-28 13:08:02 +0800

    优化

--- SwiftTemplet/UITableViewCell/UITableViewCellPayBill.swift
+++ SwiftTemplet/UITableViewCell/UITableViewCellPayBill.swift
@@ -18,17 +18,16 @@ class UITableViewCellPayBill: UITableViewCell {
                 btnDelete.isHidden = true
                 btnPay.isHidden = true
                 btnCancell.isHidden = true
-                
+
             case "2":
                 btnDelete.isHidden = false
                 btnPay.isHidden = true
                 btnCancell.isHidden = true
-                
+
             default:
                 btnDelete.isHidden = true
                 btnPay.isHidden = false
                 btnCancell.isHidden = false
-
             }
         }
     }
@@ -58,30 +57,65 @@ class UITableViewCellPayBill: UITableViewCell {
         if bounds.height <= 0.0 {
             return
         }
-        btnDelete.snp.makeConstraints { (make) in
-            make.centerY.equalToSuperview().offset(0);
-            make.right.equalToSuperview().offset(-10);
-            make.width.equalTo(65);
-            make.height.equalTo(25);
+        let btns: [UIButton] = [btnCancell, btnDelete, btnPay].filter { $0.isHidden == false }
+
+        var btn: UIView?
+        for (idx, item) in btns.reversed().enumerated() {
+            if idx == 0 {
+                item.snp.makeConstraints { (make) in
+                    make.top.equalToSuperview().offset(8);
+                    make.right.equalToSuperview().offset(-10);
+                    make.width.equalTo(65);
+                    make.bottom.equalToSuperview().offset(-8);
+                }
+            } else {
+                item.snp.makeConstraints { (make) in
+                    make.top.equalToSuperview().offset(8);
+                    make.right.equalTo(btn!.snp.left).offset(-10);
+                    make.width.equalTo(65);
+                    make.bottom.equalToSuperview().offset(-8);
+                }
+            }
+            btn = item;
         }
-        
-        btnPay.snp.makeConstraints { (make) in
-            make.edges.equalTo(btnDelete);
-        }
-        
-        btnCancell.snp.makeConstraints { (make) in
-            make.centerY.equalToSuperview().offset(0);
-            make.right.equalTo(btnDelete.snp.left).offset(-10);
-            make.width.height.equalTo(btnDelete);
-        }
-        
+                
         labelLeft.snp.makeConstraints { (make) in
             make.centerY.equalToSuperview().offset(0);
             make.left.equalToSuperview().offset(10);
-            make.right.equalTo(btnCancell.snp.left).offset(-10);
+            make.right.equalTo(btn!.snp.left).offset(-10);
             make.height.equalTo(25);
         }
     }
+//    func setupConstraint() {
+//        if bounds.height <= 0.0 {
+//            return
+//        }
+//        let btns = [btnCancell, btnDelete, btnPay].map { $0.isHidden = false }
+//
+//        btnDelete.snp.makeConstraints { (make) in
+//            make.centerY.equalToSuperview().offset(0);
+//            make.right.equalToSuperview().offset(-10);
+//            make.width.equalTo(65);
+//            make.height.equalTo(25);
+//        }
+//
+//        btnPay.snp.makeConstraints { (make) in
+//            make.edges.equalTo(btnDelete);
+//        }
+//
+//        btnCancell.snp.makeConstraints { (make) in
+//            make.centerY.equalToSuperview().offset(0);
+//            make.right.equalTo(btnDelete.snp.left).offset(-10);
+//            make.width.height.equalTo(btnDelete);
+//        }
+//
+//        labelLeft.snp.makeConstraints { (make) in
+//            make.centerY.equalToSuperview().offset(0);
+//            make.left.equalToSuperview().offset(10);
+//            make.right.equalTo(btnCancell.snp.left).offset(-10);
+//            make.height.equalTo(25);
+//        }
+//    }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -90,19 +124,21 @@ class UITableViewCellPayBill: UITableViewCell {
     
     lazy var btnDelete: UIButton = {
         let view = UIButton.create(.zero, title: "删除订单", imgName: nil, type: 1)
-        view.backgroundColor = UIColor.hexValue(0xFE6256)
+        view.setBackgroundImage(UIImage(color: .systemRed), for: .normal)
         view.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         return view
     }()
 
     lazy var btnPay: UIButton = {
         let view = UIButton.create(.zero, title: "支付", imgName: nil, type: 1)
+        view.setBackgroundImage(UIImage(color: .systemBlue), for: .normal)
         view.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         return view
     }()
 
     lazy var btnCancell: UIButton = {
         let view = UIButton.create(.zero, title: "取消订单", imgName: nil, type: 5)
+        view.setBackgroundImage(UIImage(color: .systemBlue), for: .normal)
         view.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         return view
     }()
