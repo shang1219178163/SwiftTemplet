1c71beddc3850c68a7ab8c2a9450995cb41d8a6f
Shang <shang1219178163@gmail.com>
2020-05-15 17:59:25 +0800

    新增复用视图

---
+++ SwiftTemplet/Controllers/IOPCouponFliterDropView.swift
@@ -0,0 +1,215 @@
+//
+//	IOPCouponFliterDropView.swift
+//	MacTemplet
+//
+//	Created by shang on 2020/05/13 14:11
+//	Copyright © 2020 shang. All rights reserved.
+//
+
+
+import UIKit
+
+import SnapKit
+import SwiftExpand
+
+
+@objcMembers class NNSectionDataModel: NSObject {
+    var title = ""
+    var items: [String] = []
+    var itemIndex = 0
+    var cellHeight: CGFloat = 60
+}
+        
+///
+@objc protocol IOPCouponFliterDropViewDelegate{
+    @objc func couponFliterDropView(_ view: IOPCouponFliterDropView, btn: UIButton);
+    
+}
+
+@objcMembers class IOPCouponFliterDropView: UIView {
+    
+    weak var delegate: IOPCouponFliterDropViewDelegate?
+    
+    /// 当前数据源
+    lazy var list: [NNSectionDataModel] = []
+    
+    // MARK: - 视图展示必传属性
+    weak var parController: UIViewController?
+    var sender: UIView?{
+        willSet{
+            guard let newValue = newValue else { return }
+            let rect = parController!.view.convert(newValue.frame, to: parController!.view)
+            self.containView.originY = rect.maxY;
+        }
+    }
+    // MARK: - lifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+//        _ = addGestureTap(self, action: #selector(dismiss))
+    }
+    
+    convenience init(frame: CGRect, parController: UIViewController!) {
+        self.init(frame: frame)
+        self.parController = parController
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    // MARK: - show
+    func show() {
+        self.parController!.view.insertSubview(self.containView, at: 1)//必须保持在表视图之上
+//        DDLog(self.parController!.view.subviews);
+        self.tableView.transform = self.tableView.transform.translatedBy(x: 0, y: -self.tableView.sizeHeight)
+
+        self.containView.alpha = 0.0
+        UIView.animate(withDuration: kDurationShow, animations: {
+            self.containView.alpha = 1.0
+            
+            self.tableView.transform = .identity
+
+        }, completion: nil)
+    }
+    ///  释放视图
+    func dismiss() {
+        UIView.animate(withDuration: kDurationShow, animations: {
+            self.containView.alpha = 0.0
+            
+            self.tableView.transform = self.tableView.transform.translatedBy(x: 0, y: -self.tableView.sizeHeight)
+//            self.btn.imageView?.transform = .identity;
+
+        }, completion:{ (isFinish:Bool) in
+//            self.containView.removeFromSuperview()
+            
+        })
+    }
+    
+    /// 重置筛选框
+    func setupDefaultCellFliter() {
+
+        
+    }
+    /// 重置时长框
+    func setupDefaultCellTimeDuration() {
+
+    }
+    
+    //MARK: -lazy
+    lazy var containView: UIView = {
+        var view = UIView(frame: parController!.view.bounds)
+//        var view = UIView(frame: CGRectMake(0, 50, parController!.view.bounds.width, parController!.view.bounds.height))
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.2);
+
+        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height*0.5)
+        view.addSubview(tableView)
+        
+        return view
+    }()
+    
+    lazy var tableView: UITableView = {
+        var table = UITableView.create(bounds, style: .grouped, rowHeight: kH_CellHeight)
+        table.dataSource = self
+        table.delegate = self
+        table.tableFooterView = self.footerView;
+        table.separatorStyle = .none;
+        table.backgroundColor = UIColor.clear;
+        return table
+    }()
+    
+    lazy var footerView: UIView = {
+        let view = UIButton.createGroupView(CGRectMake(0, 0, kScreenWidth, 50), list: [kTitleDefault, kTitleSure], numberOfRow: 2, padding: 0) { (control) in
+            guard let sender = control as? UIButton else { return }
+//            DDLog(sender.currentTitle)
+            self.delegate?.couponFliterDropView(self, btn: sender)
+            if sender.currentTitle == kTitleDefault {
+                self.setupDefaultCellFliter()
+                self.setupDefaultCellTimeDuration()
+            } else {
+                self.dismiss();
+            }
+        }
+        
+        if let btnDefault = view.viewWithTag(0) as? UIButton {
+            btnDefault.setTitleColor(.textColor3, for: .normal)
+        }
+        
+        if let btnSure = view.viewWithTag(1) as? UIButton {
+            btnSure.setTitleColor(.white, for: .normal)
+            btnSure.setBackgroundImage(UIImage(color: .theme), for: .normal)
+        }
+        return view
+    }()
+
+}
+
+extension IOPCouponFliterDropView: UITableViewDataSource, UITableViewDelegate {
+
+    //    MARK: - tableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return list.count;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    };
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let sectionModel = list[indexPath.section] as NNSectionDataModel? else { return tableView.rowHeight }
+        return sectionModel.cellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let sectionModel = list[indexPath.section] as NNSectionDataModel? else { return UITableViewCell.dequeueReusableCell(tableView) }
+
+        let cell = IOPTableViewCellGroupView.dequeueReusableCell(tableView);
+        cell.parkGroupView.items = sectionModel.items
+        cell.parkGroupView.selectedIdxList = [0]
+        cell.parkGroupView.hideImage = true
+        
+        cell.parkGroupView.block { (groupView, control) in
+            guard let sender = control as? UIButton else { return }
+            DDLog(sender.currentTitle)
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let sectionModel = list[indexPath.section] as NNSectionDataModel? else { return }
+//        btn.setTitle(sectionModel.title, for: .normal)
+//        dismiss()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 35;
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let sectionModel = list[section] as NNSectionDataModel? else { return UIView() }
+
+        let label = UILabel(frame: .zero);
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.backgroundColor = .white;
+        label.textColor = UIColor.gray
+        label.text = "    \(sectionModel.title)"
+        return label;
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01;
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let label = UILabel(frame: .zero);
+        //        label.backgroundColor = .green;
+        //        label.text = "header\(section)";
+        return label;
+    }
+}
---
+++ SwiftTemplet/UITableViewCell/UITableViewCellChoose.swift
@@ -0,0 +1,131 @@
+//
+//  UITableViewCellChoose.swift
+//  IntelligentOfParking
+//
+//  Created by Bin Shang on 2020/3/3.
+//  Copyright © 2020 Xi'an iRain IoT. Technology Service CO., Ltd. . All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+/*
+ 勾选框   文字+文字
+         文字+文字
+ */
+class UITableViewCellChoose: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier);
+        
+        contentView.addSubview(btn);
+
+        contentView.addSubview(labelRight);
+        contentView.addSubview(labelRightSub);
+        contentView.addSubview(labelLeft);
+        contentView.addSubview(labelLeftSub);
+        
+        btn.backgroundColor = UIColor.white
+        btn.setBackgroundImage(UIImage(named: "btn_selected_NO"), for: .normal)
+        btn.setBackgroundImage(UIImage(named: "btn_selected_YES"), for: .selected)
+
+        labelLeft.textColor = UIColor.black;
+        labelLeft.font = UIFont.systemFont(ofSize: 15)
+        labelLeft.adjustsFontSizeToFitWidth = true
+        
+        labelLeftSub.textColor = UIColor.textColor6;
+        labelRightSub.textColor = UIColor.textColor6;
+
+        labelRight.textColor = UIColor.black;
+        labelRight.textAlignment = .right;
+        labelRight.font = UIFont.systemFont(ofSize: 13)
+
+        labelRightSub.adjustsFontSizeToFitWidth = true
+        labelRightSub.textAlignment = .right;
+        labelRightSub.font = UIFont.systemFont(ofSize: 13)
+
+        labelLeft.text = "车牌号码"
+        labelLeftSub.text = "支付方式"
+        labelRight.text = "$0.00"
+        labelRightSub.text = DateFormatter.stringFromDate(Date());
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews();
+        
+        setupConstraint()
+    }
+    
+    func setupConstraint() {
+        if CGRect.zero.equalTo(bounds) == true {
+            return;
+        }
+        
+        let height: CGFloat = bounds.height - 20
+                
+        btn.snp.makeConstraints { (make) in
+            make.centerY.equalToSuperview();
+            make.left.equalToSuperview().offset(10);
+            make.width.height.equalTo(20);
+        }
+        
+        labelRight.sizeToFit();
+        labelRight.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(10);
+            make.right.equalToSuperview().offset(-10);
+            make.height.equalTo(height*0.5);
+        }
+        
+        labelRightSub.sizeToFit();
+        labelRightSub.snp.makeConstraints { (make) in
+            make.right.equalTo(labelRight);
+            make.bottom.equalToSuperview().offset(-10);
+            make.height.equalTo(labelRight);
+        }
+
+        if btn.isHidden {
+            labelLeft.snp.makeConstraints { (make) in
+                make.centerY.equalTo(labelRight);
+                make.left.equalToSuperview().offset(10);
+                make.right.equalTo(labelRight.snp.left).offset(-8);
+                make.height.equalTo(labelRight);
+            }
+            
+            labelLeftSub.snp.makeConstraints { (make) in
+                make.centerY.equalTo(labelRightSub);
+                make.left.equalTo(labelLeft);
+                make.right.equalTo(labelRightSub.snp.left).offset(-8);
+                make.height.equalTo(labelRightSub);
+            }
+
+        } else {
+            labelLeft.snp.makeConstraints { (make) in
+                make.centerY.equalTo(labelRight);
+                make.left.equalTo(btn.snp.right).offset(10);
+                make.right.equalTo(labelRight.snp.left).offset(-8);
+                make.height.equalTo(labelRight);
+            }
+            
+            labelLeftSub.snp.makeConstraints { (make) in
+                make.centerY.equalTo(labelRightSub);
+                make.left.equalTo(labelLeft);
+                make.right.equalTo(labelRightSub.snp.left).offset(-8);
+                make.height.equalTo(labelRightSub);
+            }
+        }
+        
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
+
