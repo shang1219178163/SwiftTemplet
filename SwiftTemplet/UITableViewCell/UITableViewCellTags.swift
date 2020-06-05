b7d10f1f11f0c97104f7f1d94f31b49f3fa8c321
Shang <shang1219178163@gmail.com>
2020-06-05 20:16:55 +0800

    新增 tag 视图

---
+++ SwiftTemplet/UITableViewCell/UITableViewCellTags.swift
@@ -0,0 +1,56 @@
+//
+//  UITableViewCellTags.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/6/2.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+import SwiftExpand
+
+/// 星期多选
+class UITableViewCellTags: UITableViewCell {
+     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+         super.init(style: style, reuseIdentifier: reuseIdentifier);
+        
+        tagView.frame = CGRect(x: 10, y: 10, width: kScreenWidth - 20, height: self.contentView.bounds.height)
+        contentView.addSubview(tagView);
+        
+        tagView.tags = ["默认标签", "默认标签1", "默认标签2", ]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+//        tagView.snp.makeConstraints { (make) in
+//            make.top.equalToSuperview().offset(10);
+//            make.left.equalToSuperview().offset(15);
+//            make.right.equalToSuperview().offset(-15);
+//            make.bottom.equalToSuperview().offset(-10);
+//        }
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    lazy var tagView: NNTagView = {
+        var view = NNTagView(frame: self.contentView.bounds)
+        view.fontSize = 9;
+        view.textColorNormal = .orange;
+        view.tag = 2010115;
+        return view;
+    }()
+
+}
