8fe37b64e8d7e0aeec44fa4021901fc393c9ef35
Shang <shang1219178163@gmail.com>
2020-06-10 13:26:59 +0800

    新增视图

---
+++ SwiftTemplet/UIView/NNChoiceView.swift
@@ -0,0 +1,108 @@
+//
+//	NNChoiceView.swift
+//	MacTemplet
+//
+//	Created by shang on 2020/06/08 11:29
+//	Copyright © 2020 shang. All rights reserved.
+//
+
+
+import UIKit
+
+import SnapKit
+        
+///
+class NNChoiceView: UIView {
+
+    
+    typealias ViewClick = (NNChoiceView, Int) -> Void;
+    var viewBlock: ViewClick?;
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(label);
+        addSubview(groupView);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if bounds.height <= 0.0 {
+            return;
+        }
+        if label.isHidden {
+            groupView.snp.makeConstraints { (make) in
+                make.top.equalToSuperview().offset(10);
+                make.left.equalToSuperview().offset(15);
+                make.right.equalToSuperview().offset(-15);
+                make.bottom.equalToSuperview().offset(-10);
+            }
+            return
+        }
+        
+        label.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(5);
+            make.left.equalToSuperview().offset(15);
+            make.right.equalToSuperview().offset(-10);
+            make.height.lessThanOrEqualTo(25);
+        }
+        
+        groupView.snp.makeConstraints { (make) in
+            make.top.equalTo(label.snp.bottom).offset(5);
+            make.left.equalToSuperview().offset(15);
+            make.right.equalToSuperview().offset(-15);
+            make.bottom.equalToSuperview().offset(-10);
+        }
+    }
+    
+    // MARK: - funtions
+    func block(_ action: @escaping ViewClick) {
+        self.viewBlock = action;
+    }
+    
+        
+    //MARK: - lazy
+    
+    lazy var label: UILabel = {
+        let view = UILabel(frame: .zero);
+        view.text = "选择";
+        view.textColor = .gray;
+        view.font = UIFont.systemFont(ofSize: 12)
+        
+        view.numberOfLines = 1
+        
+        return view;
+    }();
+    
+    lazy var groupView: NNButtonGroupView = {
+        var view = NNButtonGroupView(frame: .zero)
+        view.padding = 10;
+        view.numberOfRow = 4;
+        
+        view.cornerRadius = 3.0
+        view.fontSize = 12
+        view.isMutiChoose = true;
+        view.hasLessOne = true
+        
+        view.iconLocation = .rightBottom
+        
+        view.titleColor = .hexValue(0x999999)
+        view.backgroudImage = UIImage(color: .hexValue(0xF3F3F3))
+
+        view.selectedTitleColor = .systemBlue
+        view.selectedBackgroudImage = UIImage(named: "btn_selected_multiple")!
+        
+        view.block({ (itemsView, sender) in
+            if let btn = sender as UIButton? {
+                print(btn.titleLabel?.text as Any, itemsView.selectedList.count, itemsView.selectedIdxList.count)
+
+            }
+        })
+        return view;
+    }()
+}
---
+++ SwiftTemplet/UIView/NNCircleProgressView.swift
@@ -0,0 +1,149 @@
+//
+//  FCCycleView.swift
+//  Study
+//
+//  Created by wyzw on 2018/4/10.
+//  Copyright © 2018年 wyzw. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol NNCircleProgressViewDelegate: NSObjectProtocol {
+    ///value等于1的时候的代理
+    func circleProgressView(_ view: NNCircleProgressView?)
+}
+
+///圆形进度条
+@objcMembers class NNCircleProgressView: UIView {
+    
+    weak var delegate: NNCircleProgressViewDelegate?
+    var block: ((NNCircleProgressView)->Void)?
+
+    ///线宽
+    var strokeWidth: CGFloat = 5{
+        willSet{
+            topLayer.lineWidth = newValue
+            bottomLayer.lineWidth = newValue
+        }
+    }
+    var bottomStrokeColor: UIColor = .lightGray{
+        willSet{
+            bottomLayer.strokeColor = newValue.cgColor
+        }
+    }
+    var topStrokeColor: UIColor = .systemBlue{
+        willSet{
+            topLayer.strokeColor = newValue.cgColor
+        }
+    }
+        
+    var radius: CGFloat = 0//半径
+    
+    //进度值0-1.0之间
+    var value: CGFloat = 0.0{
+        willSet{
+            changeValue(newValue, animated: true)
+        }
+    }
+    
+    //进度label
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .center
+        
+        label.text = "0.00";
+        label.textColor = .systemBlue;
+        label.backgroundColor = UIColor.clear
+        return label
+    }()
+    
+    private lazy var bottomLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = UIColor.lightGray.cgColor
+        layer.lineWidth = 3
+
+        return layer
+    }()
+    
+    private lazy var topLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.lineCap = .round
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = UIColor.systemBlue.cgColor
+        layer.lineWidth = 3
+
+        return layer
+    }()
+    
+    // MARK: -lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+        
+        layer.addSublayer(bottomLayer)
+        layer.addSublayer(topLayer)
+        addSubview(label)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(reloadValue))
+        addGestureRecognizer(tap)
+    }
+    
+    @objc func reloadValue() {
+        changeValue(value, animated: true)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let center = CGPoint(x: bounds.size.width/2, y: bounds.size.height/2)
+
+        label.frame = CGRect(x: 0, y: 0, width: bounds.size.width - strokeWidth*2, height: 20)
+        label.center = center
+        
+        radius = bounds.size.width/2 - strokeWidth/2
+        let bottomPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi*2), clockwise: true)
+        bottomLayer.path = bottomPath.cgPath
+        
+        changeValue(value, animated: true)
+    }
+    
+    // MARK: -funtions
+    @objc func changeValue(_ newValue: CGFloat, animated: Bool) {
+        delegate?.circleProgressView(self)
+        block?(self)
+        
+        let origin = CGPoint(x: bounds.size.width/2, y: bounds.size.height/2)
+        let radius = bounds.size.width/2 - strokeWidth/2
+
+        label.text = "\(newValue * 100)" + "%"
+        let startAngle: CGFloat = -CGFloat(Double.pi/2)
+        let endAngle: CGFloat = startAngle + newValue * CGFloat(Double.pi * 2)
+        let topPath = UIBezierPath(arcCenter: origin,
+                                    radius: radius,
+                                    startAngle: startAngle,
+                                    endAngle: endAngle,
+                                    clockwise: true)
+        topLayer.path = topPath.cgPath//添加路径
+        if animated == false {
+            return
+        }
+        //添加动画
+        let pathAnimation = CABasicAnimation.init(keyPath: "strokeEnd")
+        pathAnimation.duration = 1//动画持续时间
+        pathAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        pathAnimation.fromValue = 0
+        pathAnimation.toValue = 1
+        topLayer.add(pathAnimation, forKey: "strokeEndAnimation")
+    }
+    
+    
+    
+}
+
+
