012cde6b357e6671aeb6cb61b15b99d9133b27ab
Shang <shang1219178163@gmail.com>
2020-08-01 14:09:04 +0800

    左右联动列表

---
+++ SwiftTemplet/Vendor/leftright/LeftRightTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  LeftRightTableViewController.h
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/8/1.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+///左右联动列表
+@interface LeftRightTableViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
