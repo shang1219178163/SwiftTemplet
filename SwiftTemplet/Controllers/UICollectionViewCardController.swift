27c318ba66768a5c4e89c44aa6fbdcc505d43df8
Shang <shang1219178163@gmail.com>
2020-08-16 21:09:04 +0800

    新增卡片样式

---
+++ SwiftTemplet/Controllers/UICollectionViewCardController.swift
@@ -0,0 +1,86 @@
+//
+//  UICollectionViewCardController.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/8/11.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+import SwiftExpand
+
+class UICollectionViewCardController: UIViewController {
+
+    lazy var flowLayout: NNCollectionViewCardFlowLayout = {
+        let layout = NNCollectionViewCardFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        let itemW = kScreenWidth*0.9
+        layout.itemSize = CGSize(width: itemW, height: 170)
+
+        return layout;
+    }()
+    
+    lazy var ctView: UICollectionView = {
+        let view = UICollectionView.create(self.view.bounds, layout: self.flowLayout)
+        view.backgroundColor = .white
+        view.isPagingEnabled = false
+
+        view.delegate = self
+        view.dataSource = self
+        return view
+    }()
+    
+    var dataList = [String]()
+    
+    // MARK: -lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+         view.backgroundColor = .white
+                    
+        for j in 0 ..< 5 {
+            dataList.append("\(j)")
+        }
+        
+         // 注册cell
+         ctView.dictClass = [UICollectionView.elementKindSectionHeader: ["UICTReusableViewOne", "UICTViewCellSubtitle"],
+                             UICollectionView.elementKindSectionFooter: ["UICTReusableViewZero", "UICTViewCellValue"],
+                             UICollectionView.elementKindSectionItem: ["UICTViewCellZero","UICTViewCellOne"],
+         ]
+         view.addSubview(ctView)
+    }
+    
+
+
+}
+
+extension UICollectionViewCardController: UICollectionViewDataSource, UICollectionViewDelegate{
+    //MARK: --UICollectionView
+   func numberOfSections(in collectionView: UICollectionView) -> Int {
+       return 1
+   }
+
+   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return dataList.count
+   }
+
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(for: UICTViewCellZero.self, indexPath: indexPath)
+        cell.lineBottom.isHidden = false
+        cell.lineRight.isHidden = false
+    
+       cell.imgView.backgroundColor = .random
+       cell.lab.text = String(format:"%ditem",indexPath.row)
+    
+        cell.getViewLayer()
+       return cell
+   }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
---
+++ SwiftTemplet/UICollectionViewCell/NNCollectionViewCardFlowLayout.swift
@@ -0,0 +1,63 @@
+//
+//  CyclicCardFlowLayout.swift
+//  CyclicCard
+//
+//  Created by Tony on 17/1/11.
+//  Copyright © 2017年 Tony. All rights reserved.
+//
+
+import UIKit
+
+class NNCollectionViewCardFlowLayout: UICollectionViewFlowLayout {
+        
+    let activeDistance: CGFloat = 200 //垂直缩放除以系数
+    let scaleFactor: CGFloat = 0.10     //缩放系数  越大缩放越大
+    
+//    override func prepare() {
+//        super.prepare()
+//   
+//    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        let array = super.layoutAttributesForElements(in: rect)
+        var visibleRect = CGRect()
+        visibleRect.origin = self.collectionView!.contentOffset
+        visibleRect.size = self.collectionView!.bounds.size
+        
+        for attributes in array! {
+            let distance = visibleRect.midX - attributes.center.x
+            let normalizedDistance = abs(distance / activeDistance)
+            let zoom = 1 - scaleFactor * normalizedDistance
+            attributes.transform3D = CATransform3DMakeScale(1.0, zoom, 1.0)
+//            let alpha = 1 - normalizedDistance
+//            attributes.alpha = alpha
+        }
+        return array
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        //滑动放大缩小  需要实时刷新layout
+        return true
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        
+        let targetRect = CGRect(x: proposedContentOffset.x, y: 0.0, width:  self.collectionView!.bounds.size.width, height: self.collectionView!.bounds.size.height)
+        // 目标区域中包含的cell
+        let attriArray = super.layoutAttributesForElements(in: targetRect)! as [UICollectionViewLayoutAttributes]
+        // collectionView落在屏幕中点的x坐标
+        let horizontalCenterX = proposedContentOffset.x + (self.collectionView!.bounds.width / 2.0)
+        var offsetAdjustment = CGFloat(MAXFLOAT)
+        for layoutAttributes in attriArray {
+            let itemHorizontalCenterX = layoutAttributes.center.x
+            // 找出离中心点最近的
+            if(abs(itemHorizontalCenterX-horizontalCenterX) < abs(offsetAdjustment)) {
+                offsetAdjustment = itemHorizontalCenterX-horizontalCenterX
+            }
+        }
+        
+        //返回collectionView最终停留的位置
+        return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+    }
+}
