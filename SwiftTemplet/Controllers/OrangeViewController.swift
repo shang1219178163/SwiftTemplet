0f2b15fe794272e426e4be14cd99f4c3c5a7cfcf
Shang <shang1219178163@gmail.com>
2020-09-11 11:18:56 +0800

    转场动画

--- SwiftTemplet/Controllers/OrangeViewController.swift
+++ SwiftTemplet/Controllers/OrangeViewController.swift
@@ -10,21 +10,77 @@ import UIKit
 
 class OrangeViewController: UIViewController {
 
+    lazy var btn: UIButton = {
+        let view = UIButton.create(title: "上推", textColor: .white, backgroundColor: .clear)
+        return view
+    }()
+    
+    lazy var transitionDelegate: NNTransitionDelegate = {
+        let delgate = NNTransitionDelegate(presented: NNPullDownAnimator(), dismissed: NNPushUpAnimator())
+        return delgate
+    }()
+     // MARK: -lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        edgesForExtendedLayout = []
+        title = "OrangeViewController"
+        view.backgroundColor = UIColor.systemOrange
+        transitioningDelegate = transitionDelegate
+        
+        view.addSubview(btn)
+        
+        let panUp = UIPanGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+//        panUp.delegate = self
+        view.addGestureRecognizer(panUp)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        btn.sizeToFit()
+        btn.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
     }
-    */
+    
+    // MARK: -funtions
+    let interaction = UIPercentDrivenInteractiveTransition()
+    
+    @objc func handleGesture(_ gesture: UIPanGestureRecognizer) {
+        let translate = gesture.translation(in: gesture.view)
+        let percent   = -translate.y / gesture.view!.bounds.size.height
+        
+        if gesture.state == .began {
+            transitionDelegate.interaction = interaction
+            
+            dismiss(animated: true)
+        } else if gesture.state == .changed {
+            interaction.update(percent)
+        } else if gesture.state == .ended {
+            let velocity = gesture.velocity(in: gesture.view)
+            interaction.completionSpeed = 0.999  // https://stackoverflow.com/a/42972283/1271826
+            if (percent > 0.5 && velocity.y == 0) || velocity.y < 0 {
+                interaction.finish()
+            } else {
+                interaction.cancel()
+            }
+        }
+        
+    }
+}
 
+
+extension OrangeViewController: UIGestureRecognizerDelegate {
+    
+    // make sure it only recognizes upward gestures
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let pan = gestureRecognizer as? UIPanGestureRecognizer {
+            let translation = pan.translation(in: pan.view)
+            let angle = atan2(translation.y, translation.x)
+            return abs(angle + .pi / 2.0) < (.pi / 8.0)
+        }
+        return false
+    }
 }
