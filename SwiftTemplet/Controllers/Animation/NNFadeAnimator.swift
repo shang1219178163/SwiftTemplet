81a82d0dbb9086a96e0e3bcad0eb5ec6248aedb1
Shang <shang1219178163@gmail.com>
2020-09-11 11:17:28 +0800

    动画

--- SwiftTemplet/Controllers/Animation/NNFadeAnimator.swift
+++ SwiftTemplet/Controllers/Animation/NNFadeAnimator.swift
@@ -11,3 +11,32 @@ import UIKit
 class NNFadeAnimator: NSObject {
 
 }
+
+extension NNFadeAnimator: UIViewControllerAnimatedTransitioning {
+
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let fromVC = transitionContext.viewController(forKey: .from),
+        let toVC = transitionContext.viewController(forKey: .to),
+        let fromView = transitionContext.view(forKey: .from) ?? fromVC.view,
+        let toView = transitionContext.view(forKey: .to) ?? toVC.view
+        else { return }
+        
+        let containerView = transitionContext.containerView
+        containerView.insertSubview(toView, aboveSubview: fromView)
+        
+        toView.alpha = 0
+
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            toView.alpha = 1
+            
+        }, completion: { finished in
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        })
+    }
+}
+
+
--- SwiftTemplet/Controllers/RedViewController.swift
+++ SwiftTemplet/Controllers/RedViewController.swift
@@ -8,23 +8,80 @@
 
 import UIKit
 
+///
 class RedViewController: UIViewController {
-
+    
+    lazy var btn: UIButton = {
+        let view = UIButton.create(title: "下拉", textColor: .white, backgroundColor: .clear)
+        return view
+    }()
+    // MARK: -lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        edgesForExtendedLayout = []
+        title = "RedViewController"
+        view.backgroundColor = UIColor.systemRed
+        
+        view.addSubview(btn)
+                
+        let panDown = UIPanGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
+//        panDown.delegate = self
+        view.addGestureRecognizer(panDown)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        btn.sizeToFit()
+        btn.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        }
     }
-    */
+    
+    // MARK: -funtions
+    let interaction = UIPercentDrivenInteractiveTransition()
+
+    @objc func handleGesture(_ gesture: UIPanGestureRecognizer) {
+        let translate = gesture.translation(in: gesture.view)
+        let percent   = translate.y / gesture.view!.bounds.size.height
+        
+        if gesture.state == .possible {
+            if translate != .zero {
+                let angle = atan2(translate.y, translate.x)
+                print(angle)
+            }
+        } else if gesture.state == .began {
+            let controller = OrangeViewController()
+            controller.transitionDelegate.interaction = interaction
+            
+            present(controller, animated: true)
+        } else if gesture.state == .changed {
+            interaction.update(percent)
+        } else if gesture.state == .ended || gesture.state == .cancelled {
+            let velocity = gesture.velocity(in: gesture.view)
+            interaction.completionSpeed = 0.999  // https://stackoverflow.com/a/42972283/1271826
+            if (percent > 0.5 && velocity.y == 0) || velocity.y > 0 {
+                interaction.finish()
+            } else {
+                interaction.cancel()
+            }
+        }
+    }
+
+}
+
+extension RedViewController: UIGestureRecognizerDelegate {
+
+    // make sure it only recognizes downward gestures
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let pan = gestureRecognizer as? UIPanGestureRecognizer {
+            let translation = pan.translation(in: pan.view)
+            let angle = atan2(translation.y, translation.x)
+            return abs(angle - .pi / 2.0) < (.pi / 8.0)
+        }
+        return false
+    }
 
 }
