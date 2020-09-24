ee1c481528b44df87646660079e159431ef8011b
Shang <shang1219178163@gmail.com>
2020-09-24 19:22:13 +0800

    九宫格视图

---
+++ SwiftTemplet/Controllers/NNSudokuViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NNSudokuViewController.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/8/22.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+import SwiftExpand
+
+class NNSudokuViewController: UIViewController {
+
+    lazy var sudokuView: NNSudokuView = {
+        let view = NNSudokuView()
+        view.numOfRow = 3
+        view.row = 3
+        view.itemType = NNButton.self
+//            view.items.forEach { $0.setTitleColor(.systemBlue, for: .normal)}
+        view.items.forEach {
+
+            guard let sender = $0 as? NNButton else { return }
+            sender.direction = .top
+            sender.setImage(UIImage(named: "btn_selected_YES"), for: .normal)
+
+            sender.addActionHandler({ control in
+                guard let sender = control as? UIButton else { return }
+                DDLog(sender.tag)
+
+            }, for: .touchUpInside)
+        }
+        
+        view.showDividers = true
+        view.dividerColor = .red
+        view.dividerWidth = 2
+        view.dividerSpacing = 10
+        return view
+    }()
+
+    // MARK: -lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupExtendedLayout()
+        view.backgroundColor = .white
+        view.addSubview(sudokuView)
+        
+        view.getViewLayer()
+    }
+    
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sudokuView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(10);
+            make.left.equalToSuperview().offset(10);
+            make.right.equalToSuperview().offset(-10);
+            make.height.equalTo(300);
+        }
+    }
+
+}
