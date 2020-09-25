da27a1e333c5458d5e0184eca53c832929424c30
Shang <shang1219178163@gmail.com>
2020-09-25 20:10:39 +0800

    新视图

---
+++ SwiftTemplet/Controllers/KOP/UIView/UITableViewCellArticle.swift
@@ -0,0 +1,24 @@
+//
+//  UITableViewCellArticle.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/9/25.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewCellArticle: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
