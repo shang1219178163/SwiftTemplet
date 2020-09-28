bd25423e49e4336a9d1727b48b67e7ebd0229d73
Shang <shang1219178163@gmail.com>
2020-09-28 19:37:00 +0800

    二维码生成

--- SwiftTemplet/Controllers/QRCodeViewController.swift
+++ SwiftTemplet/Controllers/QRCodeViewController.swift
@@ -7,24 +7,44 @@
 //
 
 import UIKit
+import SwiftExpand
 
 class QRCodeViewController: UIViewController {
 
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .systemGreen
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        edgesForExtendedLayout = []
+        view.backgroundColor = .white
+        
+        view.addSubview(imageView)
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        imageView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(20);
+            make.left.equalToSuperview().offset(20);
+            make.width.equalTo(200);
+            make.height.equalTo(200);
+        }
     }
-    */
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let size = CGSize(width: 200, height: 200)
+        let image = UIImage.generateQRCodeImage("Copyright © 2020 BN. All rights reserved.", size: size, logo: UIImage(named: "img_portrait_N"))
+//        let image = UIImage.generateQRCode("Copyright © 2020 BN. All rights reserved.", width: 200, height: 200)
+        imageView.image = image
+    }
 }
