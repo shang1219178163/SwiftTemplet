c48d97b5c5db8aff70786535db756c043aebdba8
Shang <shang1219178163@gmail.com>
2020-12-24 13:58:40 +0800

    RxCocoa 防止连续点击和输入框延迟调用

---
+++ SwiftTemplet/NetWorkService/RXCocoaHelper.swift
@@ -0,0 +1,82 @@
+//
+//  RXCocoaHelper.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2020/12/24.
+//  Copyright © 2020 BN. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class RXCocoaHelper: NSObject {
+
+}
+
+public extension Reactive where Base: UIButton {
+    var safeTap: ControlEvent<Void> {
+        return ControlEvent.init(events: tap.throttle(.milliseconds(1500), latest: false, scheduler: MainScheduler.instance))
+    }
+    ///避免连续点击(1.5 秒响应一次)
+    func safeDrive(onNext: @escaping ((Base) -> Void)) -> Disposable {
+        return self.safeTap
+            .asControlEvent()
+            .asDriver()
+            .drive {
+                onNext(self.base)
+            } onCompleted: {
+                
+            } onDisposed: {
+                
+            }
+    }
+}
+
+public extension Reactive where Base: UITextField {
+    ///避免连续调用(1.5 秒响应一次)
+    func safeDrive(_ dueTime: RxSwift.RxTimeInterval = .milliseconds(1500), onNext: @escaping ((String) -> Void)) -> Disposable {
+        return text.orEmpty
+            .asDriver()
+            .distinctUntilChanged()
+            .debounce(dueTime)
+            .drive(onNext: { (query) in
+                onNext(query)
+            }) {
+                
+            } onDisposed: {
+                
+            }
+    }
+}
+
+public extension Reactive where Base: UISearchBar{
+    ///避免连续调用(1.5 秒响应一次)
+    func safeDrive(_ dueTime: RxSwift.RxTimeInterval = .milliseconds(1500), onNext: @escaping ((String) -> Void)) -> Disposable {
+        return text.orEmpty
+            .asDriver()
+            .distinctUntilChanged()
+            .debounce(dueTime)
+            .drive(onNext: { (query) in
+                onNext(query)
+            }) {
+                
+            } onDisposed: {
+                
+            }
+    }
+}
+
+public extension UITextField{
+    ///避免连续调用(1.5 秒响应一次)
+    func rxDrive(onNext: @escaping ((String) -> Void)) -> Disposable {
+        return self.rx.safeDrive(onNext: onNext)
+    }
+}
+
+public extension UISearchBar{
+    ///避免连续调用(1.5 秒响应一次)
+    func rxDrive(onNext: @escaping ((String) -> Void)) -> Disposable {
+        return self.rx.safeDrive(onNext: onNext)
+    }
+}
