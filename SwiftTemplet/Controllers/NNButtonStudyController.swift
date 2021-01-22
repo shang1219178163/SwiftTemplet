2c4d3d8944903211da2edc17f9c30450fc4d3ec1
Shang <shang1219178163@gmail.com>
2021-01-22 18:00:27 +0800

    按钮重构，实现边框和圆角的各个状态展示

--- SwiftTemplet/Controllers/NNButtonStudyController.swift
+++ SwiftTemplet/Controllers/NNButtonStudyController.swift
@@ -12,14 +12,16 @@ import SwiftExpand
 
 class NNButtonStudyController: UIViewController{
 
+    var normlImage: UIImage = UIImage(named: "icon_selected_no_default")!
+    var seletedImage: UIImage = UIImage(named: "icon_selected_yes_green")!
+    
     lazy var btn: UIButton = {
         let view = UIButton(type: .custom)
-        view.frame = CGRect(x: 0, y: 0, width: 80, height: 35)
         view.setTitle("默认样式", for: .normal);
 
         view.setTitleColor(UIColor.red, for: .normal)
 //        view.adjustsImageWhenHighlighted = false
-//        view.titleLabel?.adjustsFontSizeToFitWidth = true
+//
         
         view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
 
@@ -28,11 +30,8 @@ class NNButtonStudyController: UIViewController{
     
     lazy var checkBox: UIButton = {
         let view = UIButton(type: .custom)
-        view.frame = CGRect(x: 0, y: 0, width: 150, height: 35)
         view.setTitle("绿肥红瘦", for: .normal);
 
-        var normlImage: UIImage = UIImage(named: "photo_cancell")!
-        var seletedImage: UIImage = UIImage(named: "photo_select")!
         view.setImage(normlImage, for: .normal)
         view.setImage(seletedImage, for: .selected)
         
@@ -42,7 +41,7 @@ class NNButtonStudyController: UIViewController{
         view.setTitleColor(seletedTextColor, for: .selected)
         
         view.adjustsImageWhenHighlighted = false
-        view.titleLabel?.adjustsFontSizeToFitWidth = true
+        
         
         view.sizeToFit()
 //        view.titleEdgeInsets = UIEdgeInsetsMake(0, -view.imageView!.bounds.width, 0, view.imageView!.bounds.width)
@@ -58,14 +57,12 @@ class NNButtonStudyController: UIViewController{
         view.isImageRight = true
         view.setTitle("蓝瘦香菇", for: .normal);
         view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
-
         return view
     }()
     
     lazy var radioButton: NNButton = {
         let view = NNButton(type:.custom);
         view.setTitle("荷塘夜色", for: .normal);
-
         view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
 
         return view
@@ -73,93 +70,147 @@ class NNButtonStudyController: UIViewController{
     
     lazy var button: NNButton = {
         var view = NNButton(type:.custom);
-        view.setTitle("浪迹天涯", for: .normal);
-        
-//        view.direction = .bottom
-//        view.iconLocation = .leftTop
-//
-//        var normlImage: UIImage = UIImage(named: "photo_cancell")!
-//        var seletedImage: UIImage = UIImage(named: "photo_select")!
-//        view.setImage(normlImage, for: .normal)
-//        view.setImage(seletedImage, for: .selected)
-        
-        view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
+//        var view = NNButton(frame: .zero)
 
+        view.setTitle("浪迹天涯", for: .normal)
+        view.setImage(normlImage, for: .normal)
+        view.setImage(seletedImage, for: .selected)
+        
+        view.setBorderColor(.lightGray, for: .normal)
+        view.setBorderColor(.systemBlue, for: .selected)
+        view.setCornerRadius(4, for: .normal)
+
+        view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
         return view
     }()
     
     lazy var buttonTop: NNButton = {
         let view = NNButton(type:.custom);
-        view.setTitle("浪迹天涯", for: .normal);
-        view.direction = .top
-        view.iconLocation = .leftTop
-
-        var normlImage: UIImage = UIImage(named: "photo_cancell")!
-        var seletedImage: UIImage = UIImage(named: "photo_select")!
+        view.setTitle("浪迹天涯", for: .normal)
+        
         view.setImage(normlImage, for: .normal)
         view.setImage(seletedImage, for: .selected)
         
-        view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
+        view.setBorderColor(.lightGray, for: .normal)
+        view.setBorderColor(.systemBlue, for: .selected)
+        view.setCornerRadius(4, for: .normal)
 
+        view.direction = .top
+        view.iconLocation = .leftTop
+        
+        view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
         return view
     }()
     
     lazy var buttonBottom: NNButton = {
-        let view = NNButton(type:.custom);
-//        view.setTitle("浪迹天涯", for: .normal);
+        let view = NNButton(type:.custom)
+        view.setTitle("浪迹天涯", for: .normal)
+
+        view.setImage(normlImage, for: .normal)
+        view.setImage(seletedImage, for: .selected)
+        
+        view.setBorderColor(.lightGray, for: .normal)
+        view.setBorderColor(.systemBlue, for: .selected)
+        view.setCornerRadius(4, for: .normal)
+
         view.direction = .bottom
         view.iconLocation = .leftBottom
 
-        var normlImage: UIImage = UIImage(named: "photo_cancell")!
-        var seletedImage: UIImage = UIImage(named: "photo_select")!
-        view.setImage(normlImage, for: .normal)
-        view.setImage(seletedImage, for: .selected)
-
         view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
-
         return view
     }()
     
     lazy var buttonRight: NNButton = {
         let view = NNButton(type:.custom);
         view.setTitle("浪迹天涯", for: .normal);
-        view.direction = .right
-        view.iconLocation = .rightBottom
-
-        var normlImage: UIImage = UIImage(named: "photo_cancell")!
-        var seletedImage: UIImage = UIImage(named: "photo_select")!
+        
         view.setImage(normlImage, for: .normal)
         view.setImage(seletedImage, for: .selected)
+        
+        view.setBorderColor(.lightGray, for: .normal)
+        view.setBorderColor(.systemBlue, for: .selected)
+        view.setCornerRadius(4, for: .normal)
+
+        view.direction = .right
+        view.iconLocation = .rightBottom
 
         view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
         return view
     }()
     
-    @objc func handActionBtn(_ sender: UIButton) {
+    lazy var buttonAdd: NNButton = {
+        let view = NNButton(type:.custom);
+        view.setTitle("浪迹天涯", for: .normal);
+        
+        view.setImage(normlImage, for: .normal)
+        view.setImage(seletedImage, for: .selected)
+        view.titleLabel?.isHidden = true
+
+        view.setBorderColor(.lightGray, for: .normal)
+        view.setBorderColor(.systemBlue, for: .selected)
+        view.setCornerRadius(4, for: .normal)
+        
+        view.direction = .none
+        view.iconLocation = .rightTop
+        
+        view.iconSize = GGSizeMake(20, 20)
+        view.iconOffset = UIOffsetMake(8, -8)
+        view.eventInsetDX = 8;
+        view.eventInsetDY = 8;
+        view.iconBtn.setBackgroundImage(UIImage(named: "icon_delete"), for: .normal)
+        view.iconBtn.addTarget(self, action: #selector(handActionDelete(_:)), for: .touchUpInside)
+
+        view.addTarget(self, action: #selector(handActionBtn(_:)), for: .touchUpInside)
+        return view
+    }()
+    
+    @objc func handActionBtn(_ sender: NNButton) {
         sender.isSelected = !sender.isSelected
-        DDLog(sender)
+        DDLog(sender.frame, sender.titleLabel?.frame)
+        
     }
 
+    @objc func handActionDelete(_ sender: UIButton) {
+        DDLog(sender)
+    }
+    
     // MARK: -life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.addSubview(button)
+        view.addSubview(buttonRight)
+        view.addSubview(buttonTop)
+        view.addSubview(buttonBottom)
+        view.addSubview(buttonAdd)
+
+        view.addSubview(btn)
         view.addSubview(checkBox)
         view.addSubview(boxButton)
         view.addSubview(radioButton)
-        view.addSubview(button)
-        view.addSubview(buttonTop)
-        view.addSubview(buttonBottom)
-        view.addSubview(buttonRight)
-        view.addSubview(btn)
 
-        view.getViewLayer()
+//        button.isHidden = true
+        checkBox.isHidden = true
+
+//        view.getViewLayer()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
                 
+        button.frame = CGRectMake(20, 20, 120, 30);
+        buttonRight.frame = CGRectMake(20, button.frame.maxY + 20, 120, 35);
+        buttonTop.frame = CGRectMake(20, buttonRight.frame.maxY + 20, 120, 35*2);
+        buttonBottom.frame = CGRectMake(20, buttonTop.frame.maxY + 20, 120, 35*2);
+        buttonAdd.frame = CGRectMake(20, buttonBottom.frame.maxY + 20, 75, 75);
+        
+        btn.frame = CGRectMake(20, buttonAdd.frame.maxY + 20, 120, 35);
+        checkBox.frame = CGRectMake(20, btn.frame.maxY + 20, 120, 35);
+        boxButton.frame = CGRectMake(20, checkBox.frame.maxY + 20,  120, 35);
+        radioButton.frame = CGRectMake(20, boxButton.frame.maxY + 20,  120, 35);
+
+        return
         checkBox.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(20);
             make.left.equalToSuperview().offset(20)
