cc65f82e29d03b73734c6a93c153f88afc0d850f
Shang <shang1219178163@gmail.com>
2021-02-26 11:37:38 +0800

    no message

--- SwiftTemplet/RecursionViewController.swift
+++ SwiftTemplet/RecursionViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import SwiftExpand
-import SwiftExpand
 
 class RecursionViewController: UIViewController {
 
@@ -17,24 +16,48 @@ class RecursionViewController: UIViewController {
 
         title = "递归子模型"
         // Do any additional setup after loading the view.
+        
+        var array = Array<String>()
+        var array1 = [String]()
+
+        var dictionary = Dictionary<String, String>()
+        var dictionary1 = [String: String]()
+
+        let a = "123456".substring(2, 2)
+        let b = "123456".substring(to: 2)
+        let c = "123456".substring(from: 2)
+        
+        let d = "123456"[0...2]
+        let e = "123456"[0..<2]
+        let f = "123456".prefix(2)
+        let g = "123456".suffix(2)
+
+        DDLog(a, b, c, d, e, f, g)
+        
+        
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
 
         parseGeojson()
     }
     
     func parseGeojson() {
-        guard let obj = Bundle.main.jsonObject(forResource: "recursion", ofType: "geojson"),
+        guard let obj = JSONSerialization.jsonObject(forResource: "recursion", ofType: "geojson"),
               let list = NSArray.yy_modelArray(with: NNPowerModel.self, json: obj) as? [NNPowerModel]
               else { return}
         DDLog(list.count)
                 
         let items = flatModels(list)
         DDLog(items.count)
+        
+        let items1 = list.flatModels()
+        DDLog(items1.count)
+        
+        let items2 = (list as NSArray).flatModels("children")
+        DDLog(items2.count)
     }
 
     func flatModels(_ items: [NNPowerModel]?) -> [NNPowerModel] {
@@ -59,16 +82,6 @@ class RecursionViewController: UIViewController {
         return list;
     }
     
-//    func recursionModel(_ model: NNPowerModel, list: inout [NNPowerModel]) {
-//        guard let children = model.children else { return }
-//        children.forEach { (child) in
-//            list.append(child)
-//
-//            if let _ = child.children {
-//                recursionModel(child, list: &list)
-//            }
-//        }
-//    }
 }
 
 
@@ -105,3 +118,6 @@ class RecursionViewController: UIViewController {
                 ]
     }
 }
+
+
+
