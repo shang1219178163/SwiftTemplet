0b3033eb5e1a6e3e3911415eb2fb14850468134f
Shang <shang1219178163@gmail.com>
2021-03-23 20:01:09 +0800

    调整

--- SwiftTemplet/NSObject+Helper.swift
+++ SwiftTemplet/NSObject+Helper.swift
@@ -75,15 +75,15 @@ extension AVAudioSession{
 }
 
 import CommonCrypto
-//@objc public extension NSString{
-//
+@objc public extension NSString{
+
 //    func sha256(data: NSData) -> NSData {
 //        var hash = [UInt8].init(repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
 //        CC_SHA256(data.bytes, CC_LONG(data.length), &hash)
 //        let res = NSData(bytes: hash, length: Int(CC_SHA256_DIGEST_LENGTH))
 //        return res
 //    }
-//    
+//
 //    func sha1() -> NSString {
 //        let data = self.data(using: .utf8)
 //        var digest = [UInt8].init(repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
@@ -94,4 +94,30 @@ import CommonCrypto
 //         }
 //         return output as NSString
 //     }
-//}
+    
+//    func map(_ separator: String = ",", block: @escaping (()->Void)) -> String {
+//        self.components(separatedBy: separator).map { (<#String#>) -> T in
+//            <#code#>
+//        }
+//    }
+}
+
+public extension String{
+        
+//    @inlinable public func map<T>(_ transform: (Element) throws -> T) rethrows -> [T]
+//
+//    func fuckMap<T>(_ separator: String = ",", transform:(Element) throws -> T) rethrows -> [T] {
+//        let items = self.components(separatedBy: separator)
+//        var newArray = [T]()
+//        for ele in items {
+//            newArray.append(try transform(ele))
+//        }
+//        return newArray
+//    }
+}
+
+@objc public extension NSAttributedString{
+        
+
+        
+}
