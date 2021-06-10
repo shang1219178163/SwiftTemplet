6e3881e09c901e494e12db00637c760baba79ee2
Shang <shang1219178163@gmail.com>
2021-06-10 11:00:39 +0800

    支付策略

---
+++ SwiftTemplet/OrderPayProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  OrderPayProtocol.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2021/6/9.
+//  Copyright © 2021 BN. All rights reserved.
+//
+
+import UIKit
+
+class OrderPayProtocol: NSObject {
+
+}
