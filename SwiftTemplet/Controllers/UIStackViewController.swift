ffefa5bdea58a74ff10860ed5f4a8a7a2c5fa549
Shang <shang1219178163@gmail.com>
2021-07-25 11:03:40 +0800

    update

--- SwiftTemplet/Controllers/UIStackViewController.swift
+++ SwiftTemplet/Controllers/UIStackViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftExpand
 
 /// textView.isScrollEnabled = false
 class UIStackViewController: UIViewController {
@@ -14,11 +15,16 @@ class UIStackViewController: UIViewController {
     lazy var stackView: UIStackView = {
         //创建StackView
         let stackView = UIStackView()
+        stackView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         //设置子视图间隔
         stackView.spacing = 10
+        
         //子视图的高度或宽度保持一致
 //        stackView.distribution = .fillProportionally
-        stackView.backgroundColor = UIColor.lightBlue
+//        stackView.backgroundColor = UIColor.lightBlue
+        
+        stackView.distribution = .fill
+        stackView.alignment = .trailing
 
         return stackView;
     }()
@@ -28,14 +34,32 @@ class UIStackViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        edgesForExtendedLayout = []
+        view.backgroundColor = .white
         
-        let images = [UIImage(named: "Skull.jpg"), UIImage(named: "Skull.jpg"), UIImage(named: "Skull.jpg")]
-        for image in images {
-            //创建并添加内部视图
-            let imageView = UIImageView(image: image)
-            imageView.contentMode = .scaleAspectFill
-            imageView.clipsToBounds = true
-            stackView.addArrangedSubview(imageView)
+        
+//        [UIImage(named: "Skull.jpg"),
+//         UIImage(named: "Skull.jpg"),
+//         UIImage(named: "Skull.jpg")]
+//            .map { image in
+//                let imageView = UIImageView(image: image)
+//                imageView.contentMode = .scaleAspectFill
+//                imageView.clipsToBounds = true
+//                return imageView
+//            }
+//            .forEach {
+//                stackView.addArrangedSubview($0)
+//            }
+        ["取消", "删除", "扩容", "减配", "续费", "加购"]
+            .map { e in
+            let sender = UIButton(type: .custom)
+            sender.setTitle(e, for: .normal)
+            sender.addTarget(self, action: #selector(self.handleActionBtn(_:)), for: .touchUpInside)
+            sender.sizeToFit()
+            sender.setBackgroundColor(.systemBlue, for: .normal)
+            return sender
+        }.forEach {
+            stackView.addArrangedSubview($0)
         }
          
         //将StackView添加到主视图中
@@ -50,73 +74,28 @@ class UIStackViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-         
-        let padding: CGFloat = 10
-         
-        //横竖屏判断
-        let orientation = UIApplication.shared.statusBarOrientation
-        //如果是竖屏
-        if orientation.isPortrait {
-            stackView.frame = CGRect(x: padding,
-                                     y: 64 + padding,
-                                     width: view.frame.width - padding * 2,
-                                     height: view.frame.height - 64 - padding * 2)
-//            //内部视图垂直排列
-//            stackView.axis = .vertical
-        }
-        //如果是横屏
-        else {
-            stackView.frame = CGRect(x: padding,
-                                     y: 32 + padding,
-                                     width: view.frame.width - padding * 2,
-                                     height: view.frame.height - 32 - padding * 2)
-//            //内部视图水平排列
-//            stackView.axis = .horizontal
-        }
-        //内部视图水平排列
-        stackView.axis = .vertical
-        stackView.setSubViewMultiplier(0.1, at: 0)
-        stackView.setSubViewMultiplier(0.1, at: 1)
 
     }
     
-//    override func viewWillLayoutSubviews() {
-//        super.viewWillLayoutSubviews()
-//
-//        let padding: CGFloat = 10
-//
-//        //横竖屏判断
-//        let orientation = UIApplication.shared.statusBarOrientation
-//        //如果是竖屏
-//        if orientation.isPortrait {
-//            stackView.frame = CGRect(x: padding,
-//                                     y: 64 + padding,
-//                                     width: view.frame.width - padding * 2,
-//                                     height: view.frame.height - 64 - padding * 2)
-////            //内部视图垂直排列
-////            stackView.axis = .vertical
-//        }
-//        //如果是横屏
-//        else {
-//            stackView.frame = CGRect(x: padding,
-//                                     y: 32 + padding,
-//                                     width: view.frame.width - padding * 2,
-//                                     height: view.frame.height - 32 - padding * 2)
-////            //内部视图水平排列
-////            stackView.axis = .horizontal
-//        }
-//        //内部视图水平排列
-//        stackView.axis = .vertical
-//        stackView.setSubViewMultiplier(0.1, at: 0)
-//        stackView.setSubViewMultiplier(0.1, at: 1)
-//
-//    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        stackView.axis = UIApplication.shared.statusBarOrientation.isPortrait ? .vertical : .horizontal
+        stackView.axis = .horizontal
+        stackView.frame = self.view.bounds.inset(by: UIEdgeInsetsMake(10, 10, 10, 10))
+        
+        stackView.setSubViewMultiplier(0.1, at: 0)
+        stackView.setSubViewMultiplier(0.1, at: 1)
+    }
+    
+    
     
 
     // MARK: -funtsion
     
-    func test() {
-        
+    @objc func handleActionBtn(_ sender: UIButton) {
+        DDLog(sender.currentTitle)
     }
   
 }
