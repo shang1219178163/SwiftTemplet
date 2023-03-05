684cfcc1c6ed7772c0ba9fc2dcc79cc549644a9c
Shang <shangbinbin@haier.com>
2023-03-05 12:01:58 +0800

    更新

--- SwiftTemplet/Controllers/Operator/OperatorViewController.swift
+++ SwiftTemplet/Controllers/Operator/OperatorViewController.swift
@@ -45,11 +45,41 @@ class OperatorViewController: UIViewController {
         let edg2 = UIEdgeInsets(top: 4, left: 3, bottom: 2, right: 1);
         
         let e = edg1 + edg2;
-        print("edg1: \(edg1)");//edg1: UIEdgeInsets(top: 1.0, left: 2.0, bottom: 3.0, right: 4.0)
-        print("edg2: \(edg2)");//edg2: UIEdgeInsets(top: 4.0, left: 3.0, bottom: 2.0, right: 1.0)
+//        print("edg1: \(edg1)");//edg1: UIEdgeInsets(top: 1.0, left: 2.0, bottom: 3.0, right: 4.0)
+//        print("edg2: \(edg2)");//edg2: UIEdgeInsets(top: 4.0, left: 3.0, bottom: 2.0, right: 1.0)
         print("e: \(e)");//e: UIEdgeInsets(top: 5.0, left: 5.0, bottom: 5.0, right: 5.0)
+        
+//        testHightFunc();
+//        testUrl();
     }
+    
+    func testUrl() {
+//        let urlStr: String? = "https://github.com/wangyanchang21"
+        let urlStr: String? = nil;
+        urlStr.flatMap(URL.init).map(loadURL);
+    }
+    
+    func loadURL(url: URL) {
+        print("url.absoluteString: \(url.absoluteString)")
+    }
+    
+    
+    func testHightFunc() {
+        let latticeNumbers = [[1, Optional(2), 3], [3, nil, 5]]
+        // 解析首层元素, 若没有nil, 则会降维
+        let flatMapArr = latticeNumbers.flatMap { $0 }
+        print("flatMapArr: \(flatMapArr)");//flatMapArr: [Optional(1), Optional(2), Optional(3), Optional(3), nil, Optional(5)]
+        let compactMapArr = flatMapArr.compactMap { $0 }
+        print("compactMapArr: \(compactMapArr)");//compactMapArr: [1, 2, 3, 3, 5]
 
+
+        let titles = ["aaa", "bbb", "ccc", "ddd"]
+        let numbers = [111, 222, 333]
+        let zipA = zip(titles, numbers)
+        for (title, num) in zipA {
+            print("\(title)-\(num)")
+        }
+    }
 }
 
 
