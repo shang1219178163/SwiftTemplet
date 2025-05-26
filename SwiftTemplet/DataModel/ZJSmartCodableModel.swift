7bc9a8863001d10d96e75aae408dc6807d379d25
Shang <shangbinbin@haier.com>
2025-05-26 15:50:29 +0800

    feat：SmartCodable 模型解析

---
+++ SwiftTemplet/DataModel/ZJSmartCodableModel.swift
@@ -0,0 +1,43 @@
+//
+//  ZJSmartCodableModel.swift
+//  SwiftTemplet
+//
+//  Created by Bin Shang on 2025/4/1.
+//  Copyright © 2025 BN. All rights reserved.
+//
+
+import SmartCodable
+
+struct ZJSmartCodableModel: SmartCodable {
+    var name: String?
+    var age: Int?
+    var weight: Double?
+    var sex: Int?
+    var location: String?
+    @SmartAny
+    var customData: Any?
+    var threeDayForecast: [ZJThreeDaySmartCodableModel] = []
+    
+    static func mappingForKey() -> [SmartKeyTransformer]? {
+        [
+            CodingKeys.threeDayForecast <--- "three_day_forecast",
+            CodingKeys.name <--- ["username", "name"]
+        ]
+    }
+}
+
+
+struct ZJThreeDaySmartCodableModel: SmartCodable {
+    var conditions: String?
+    var day: String?
+    var temperature: Int?
+
+    
+    static func mappingForKey() -> [SmartKeyTransformer]? {
+        [
+            CodingKeys.conditions <--- "conditions",
+            CodingKeys.day <--- ["day",],
+            CodingKeys.temperature <--- "temperature",
+        ]
+    }
+}
